e928eeeb0e9399aa062c4e7d3cc96cd772c7a82d
veidzj <veidzjv@gmail.com>
2024-02-12 11:00:45 -0300

    feat: ensure DbAddAccount calls CheckAccountByEmailRepository with correct email

--- src/application/usecases/commands/db-add-account.ts
+++ src/application/usecases/commands/db-add-account.ts
@@ -1,10 +1,15 @@
+import { type CheckAccountByEmailRepository } from '@/application/protocols/queries'
 import { type AddAccountRepository } from '@/application/protocols/commands'
 import { type AddAccount } from '@/domain/usecases/commands'
 
 export class DbAddAccount implements AddAccount {
-  constructor(private readonly addAccountRepository: AddAccountRepository) {}
+  constructor(
+    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository,
+    private readonly addAccountRepository: AddAccountRepository
+  ) {}
 
   public async add(input: AddAccount.Input): Promise<void> {
+    await this.checkAccountByEmailRepository.check(input.email)
     await this.addAccountRepository.add(input)
   }
 }
