5afa392b4174cfa6b3b3f9fc16135180850952ad
veidzj <veidzjv@gmail.com>
2024-02-12 14:55:44 -0300

    refactor: group tests

--- tests/application/usecases/commands/db-add-account.spec.ts
+++ tests/application/usecases/commands/db-add-account.spec.ts
@@ -37,53 +37,59 @@ const mockInput = (): AddAccount.Input => ({
 })
 
 describe('DbAddAccount', () => {
-  test('Should call CheckAccountByEmailRepository with correct email', async() => {
-    const { sut, checkAccountByEmailRepositorySpy } = makeSut()
-    const input = mockInput()
-    await sut.add(input)
-    expect(checkAccountByEmailRepositorySpy.email).toEqual(input.email)
+  describe('CheckAccountByEmailRepository', () => {
+    test('Should call CheckAccountByEmailRepository with correct email', async() => {
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
+      const input = mockInput()
+      await sut.add(input)
+      expect(checkAccountByEmailRepositorySpy.email).toEqual(input.email)
+    })
+
+    test('Should throw if CheckAccountByEmailRepository throws', async() => {
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
+      jest.spyOn(checkAccountByEmailRepositorySpy, 'check').mockRejectedValueOnce(new Error())
+      const promise = sut.add(mockInput())
+      await expect(promise).rejects.toThrow()
+    })
+
+    test('Should throw AccountAlreadyExists if CheckAccountByEmailRepository returns true', async() => {
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
+      checkAccountByEmailRepositorySpy.output = true
+      const promise = sut.add(mockInput())
+      await expect(promise).rejects.toThrow(new AccountAlreadyExists())
+    })
   })
 
-  test('Should throw if CheckAccountByEmailRepository throws', async() => {
-    const { sut, checkAccountByEmailRepositorySpy } = makeSut()
-    jest.spyOn(checkAccountByEmailRepositorySpy, 'check').mockRejectedValueOnce(new Error())
-    const promise = sut.add(mockInput())
-    await expect(promise).rejects.toThrow()
+  describe('Hasher', () => {
+    test('Should call Hasher with correct password', async() => {
+      const { sut, hasherSpy } = makeSut()
+      const input = mockInput()
+      await sut.add(input)
+      expect(hasherSpy.plainText).toBe(input.password)
+    })
+
+    test('Should throw if Hasher throws', async() => {
+      const { sut, hasherSpy } = makeSut()
+      jest.spyOn(hasherSpy, 'hash').mockRejectedValueOnce(new Error())
+      const input = mockInput()
+      const promise = sut.add(input)
+      await expect(promise).rejects.toThrow()
+    })
   })
 
-  test('Should throw AccountAlreadyExists if CheckAccountByEmailRepository returns true', async() => {
-    const { sut, checkAccountByEmailRepositorySpy } = makeSut()
-    checkAccountByEmailRepositorySpy.output = true
-    const promise = sut.add(mockInput())
-    await expect(promise).rejects.toThrow(new AccountAlreadyExists())
-  })
+  describe('AddAccountRepository', () => {
+    test('Should call AddAccountRepository with correct values', async() => {
+      const { sut, addAccountRepositorySpy } = makeSut()
+      const input = mockInput()
+      await sut.add(input)
+      expect(addAccountRepositorySpy.input).toEqual(input)
+    })
 
-  test('Should call Hasher with correct password', async() => {
-    const { sut, hasherSpy } = makeSut()
-    const input = mockInput()
-    await sut.add(input)
-    expect(hasherSpy.plainText).toBe(input.password)
-  })
-
-  test('Should throw if Hasher throws', async() => {
-    const { sut, hasherSpy } = makeSut()
-    jest.spyOn(hasherSpy, 'hash').mockRejectedValueOnce(new Error())
-    const input = mockInput()
-    const promise = sut.add(input)
-    await expect(promise).rejects.toThrow()
-  })
-
-  test('Should call AddAccountRepository with correct values', async() => {
-    const { sut, addAccountRepositorySpy } = makeSut()
-    const input = mockInput()
-    await sut.add(input)
-    expect(addAccountRepositorySpy.input).toEqual(input)
-  })
-
-  test('Should throw if AddAccountRepository throws', async() => {
-    const { sut, addAccountRepositorySpy } = makeSut()
-    jest.spyOn(addAccountRepositorySpy, 'add').mockRejectedValueOnce(new Error())
-    const promise = sut.add(mockInput())
-    await expect(promise).rejects.toThrow()
+    test('Should throw if AddAccountRepository throws', async() => {
+      const { sut, addAccountRepositorySpy } = makeSut()
+      jest.spyOn(addAccountRepositorySpy, 'add').mockRejectedValueOnce(new Error())
+      const promise = sut.add(mockInput())
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
