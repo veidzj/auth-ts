5f2b004857eba1497141fa9e5a156244dac98687
veidzj <veidzjv@gmail.com>
2024-02-13 14:08:50 -0300

    test: ensure CheckAccountByEmailMongoRepository throws if mongo throws

---
+++ tests/infra/db/mongodb/queries/check-account-by-email-mongo-repository.spec.ts
@@ -0,0 +1,33 @@
+import { Collection } from 'mongodb'
+import { faker } from '@faker-js/faker'
+
+import { connectToDatabase, disconnectFromDatabase, clearCollection, getCollection } from '@/tests/infra/db/mongodb'
+import { CheckAccountByEmailMongoRepository } from '@/infra/db/mongodb/queries'
+
+let accountCollection: Collection
+
+const makeSut = (): CheckAccountByEmailMongoRepository => {
+  return new CheckAccountByEmailMongoRepository()
+}
+
+describe('CheckAccountByEmailMongoRepository', () => {
+  beforeAll(async() => {
+    await connectToDatabase()
+  })
+
+  afterAll(async() => {
+    await disconnectFromDatabase()
+  })
+
+  beforeEach(async() => {
+    accountCollection = await getCollection('accounts')
+    await clearCollection(accountCollection)
+  })
+
+  test('Should throw if mongo throws', async() => {
+    const sut = makeSut()
+    jest.spyOn(Collection.prototype, 'countDocuments').mockRejectedValueOnce(new Error())
+    const promise = sut.check(faker.internet.email())
+    await expect(promise).rejects.toThrow()
+  })
+})
