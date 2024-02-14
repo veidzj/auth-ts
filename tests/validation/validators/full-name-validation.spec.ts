3dc3559d7a36783a1bee8cc4350987f71863b844
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 17:00:38 -0300

    test: ensure FullNameValidation adds an error if full name is greater than 50 characters long

--- tests/validation/validators/full-name-validation.spec.ts
+++ tests/validation/validators/full-name-validation.spec.ts
@@ -4,14 +4,22 @@ import { FullNameValidation } from '@/validation/validators'
 
 describe('FullNameValidation', () => {
   let shortFullName: string
+  let longFullName: string
 
   beforeAll(() => {
     shortFullName = faker.string.alpha({ length: { min: 1, max: 2 } })
+    longFullName = faker.string.alpha({ length: 51 })
   })
 
-  test('Should add an error if username is less than 3 characters long', () => {
+  test('Should add an error if full name is less than 3 characters long', () => {
     const sut = new FullNameValidation()
     const errors = sut.validate(shortFullName)
+    expect(errors[0]).toBe('Full name must be between 3 and 50 characters long')
+  })
+
+  test('Should add an error if full name is greater than 50 characters long', () => {
+    const sut = new FullNameValidation()
+    const errors = sut.validate(longFullName)
     expect(errors[0]).toBe('Full name must be between 3 and 50 characters long')
   })
 })
