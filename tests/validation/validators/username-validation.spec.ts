127e883044b3498cb35fde8249dd72708a51dc26
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 15:14:10 -0300

    test: ensure UsernameValidation returns an error if username starts with a number

--- tests/validation/validators/username-validation.spec.ts
+++ tests/validation/validators/username-validation.spec.ts
@@ -5,10 +5,12 @@ import { UsernameValidation } from '@/validation/validators'
 describe('UsernameValidation', () => {
   let shortUsername: string
   let longUsername: string
+  let usernameStartsWithNumber: string
 
   beforeAll(() => {
     shortUsername = faker.string.alpha({ length: { min: 1, max: 2 } })
     longUsername = faker.string.alpha({ length: 17 })
+    usernameStartsWithNumber = faker.string.numeric() + faker.string.alpha(2)
   })
 
   test('Should return an error if username is less than 3 characters long', () => {
@@ -21,5 +23,11 @@ describe('UsernameValidation', () => {
     const sut = new UsernameValidation()
     const errors = sut.validate(longUsername)
     expect(errors[0]).toBe('Username must be between 3 and 16 characters long')
+  })
+
+  test('Should return an error if username starts with a number', () => {
+    const sut = new UsernameValidation()
+    const errors = sut.validate(usernameStartsWithNumber)
+    expect(errors[0]).toBe('Username must start with a letter')
   })
 })
