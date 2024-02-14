06fc2e5863a8ff6307a96e56df648017a9a7b485
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 15:08:56 -0300

    test: ensure UsernameValidation returns an error if username is greater than 16 characters long

--- tests/validation/validators/username-validation.spec.ts
+++ tests/validation/validators/username-validation.spec.ts
@@ -4,14 +4,22 @@ import { UsernameValidation } from '@/validation/validators'
 
 describe('UsernameValidation', () => {
   let shortUsername: string
+  let longUsername: string
 
   beforeAll(() => {
     shortUsername = faker.string.alpha({ length: { min: 1, max: 2 } })
+    longUsername = faker.string.alpha({ length: 17 })
   })
 
   test('Should return an error if username is less than 3 characters long', () => {
     const sut = new UsernameValidation()
     const errors = sut.validate(shortUsername)
-    expect(errors[0]).toBe('Username must be at least 3 characters long')
+    expect(errors[0]).toBe('Username must be between 3 and 16 characters long')
+  })
+
+  test('Should return an error if username is greater than 16 characters long', () => {
+    const sut = new UsernameValidation()
+    const errors = sut.validate(longUsername)
+    expect(errors[0]).toBe('Username must be between 3 and 16 characters long')
   })
 })
