38a705e7adf38981dea631c32b11b869f06bc7e4
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 17:06:23 -0300

    test: ensure FullNameValidation adds an error if full name starts with a lowercase letter

--- tests/validation/validators/full-name-validation.spec.ts
+++ tests/validation/validators/full-name-validation.spec.ts
@@ -5,10 +5,12 @@ import { FullNameValidation } from '@/validation/validators'
 describe('FullNameValidation', () => {
   let shortFullName: string
   let longFullName: string
+  let lowercaseFullName: string
 
   beforeAll(() => {
     shortFullName = faker.string.alpha({ length: { min: 1, max: 2 } })
     longFullName = faker.string.alpha({ length: 51 })
+    lowercaseFullName = faker.string.alpha({ length: 16, casing: 'lower' })
   })
 
   test('Should add an error if full name is less than 3 characters long', () => {
@@ -21,5 +23,11 @@ describe('FullNameValidation', () => {
     const sut = new FullNameValidation()
     const errors = sut.validate(longFullName)
     expect(errors[0]).toBe('Full name must be between 3 and 50 characters long')
+  })
+
+  test('Should add an error if full name starts with a lowercase letter', () => {
+    const sut = new FullNameValidation()
+    const errors = sut.validate(lowercaseFullName)
+    expect(errors[0]).toBe('Full name must start with an uppercase letter')
   })
 })
