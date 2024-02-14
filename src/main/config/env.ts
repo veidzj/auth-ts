0a12ea34c77d0de50bb1bbfa1f539bcc5ed8c214
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 09:25:22 -0300

    fix: port env type

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/auth-ts',
-  port: Number(process.env.PORT) ?? 5050,
+  port: process.env.PORT ?? 5050,
   salt: Number(process.env.SALT) ?? 12
 }
