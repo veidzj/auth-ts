293f5919d5ec6870abe33ac3ab006f794192e91d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 17:15:28 -0300

    test: ensure FullNameValidation adds all errors if username contains more than 1 error

--- tests/validation/validators/username-validation.spec.ts
+++ tests/validation/validators/username-validation.spec.ts
@@ -8,13 +8,15 @@ describe('UsernameValidation', () => {
   let usernameStartsWithNumber: string
   let usernameWithSpaces: string
   let usernameWithSpecialCharacter: string
+  let invalidUsername: string
 
   beforeAll(() => {
     shortUsername = faker.string.alpha({ length: { min: 1, max: 2 } })
-    longUsername = faker.string.alpha({ length: 17 })
+    longUsername = faker.string.alpha(17)
     usernameStartsWithNumber = faker.string.numeric() + faker.string.alpha(2)
     usernameWithSpaces = faker.string.alpha({ length: { min: 2, max: 15 } }) + ' '
     usernameWithSpecialCharacter = faker.string.alpha() + faker.string.symbol(15)
+    invalidUsername = faker.string.numeric() + faker.string.alpha(16) + ' ' + faker.string.symbol()
   })
 
   test('Should add an error if username is less than 3 characters long', () => {
@@ -45,5 +47,15 @@ describe('UsernameValidation', () => {
     const sut = new UsernameValidation()
     const errors = sut.validate(usernameWithSpaces)
     expect(errors[0]).toBe('Username must not contain spaces')
+  })
+
+  test('Should add all errors if username contains more than 1 error', () => {
+    const sut = new UsernameValidation()
+    const errors = sut.validate(invalidUsername)
+    expect(errors.length).toBe(4)
+    expect(errors[0]).toBe('Username must be between 3 and 16 characters long')
+    expect(errors[1]).toBe('Username must start with a letter')
+    expect(errors[2]).toBe('Username must not contain spaces')
+    expect(errors[3]).toBe('Username can only contain letters, digits, underscore, hyphen and dot')
   })
 })
