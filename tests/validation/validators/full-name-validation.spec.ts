c1dbd94e4b9a8203ae48741a36749fad4f963052
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 18:45:44 -0300

    test: ensure FullNameValidation adds an error if full name have an invalid space after words

--- tests/validation/validators/full-name-validation.spec.ts
+++ tests/validation/validators/full-name-validation.spec.ts
@@ -7,12 +7,14 @@ describe('FullNameValidation', () => {
   let longFullName: string
   let lowercaseFullName: string
   let invalidSpaceBetweenFullName: string
+  let invalidSpaceAfterFullName: string
 
   beforeAll(() => {
     shortFullName = faker.string.alpha({ length: { min: 1, max: 2 } })
     longFullName = faker.string.alpha(51)
     lowercaseFullName = faker.string.alpha({ length: 16, casing: 'lower' })
-    invalidSpaceBetweenFullName = faker.string.alpha(16) + '  ' + faker.string.alpha(16)
+    invalidSpaceBetweenFullName = faker.string.alpha({ casing: 'upper' }) + faker.string.alpha(16) + '  ' + faker.string.alpha(16)
+    invalidSpaceAfterFullName = faker.string.alpha({ casing: 'upper' }) + faker.string.alpha(16) + ' '
   })
 
   test('Should add an error if full name is less than 3 characters long', () => {
@@ -36,6 +38,12 @@ describe('FullNameValidation', () => {
   test('Should add an error if full name have an invalid space between words', () => {
     const sut = new FullNameValidation()
     const errors = sut.validate(invalidSpaceBetweenFullName)
+    expect(errors[0]).toBe('Full name must be separated by a single space')
+  })
+
+  test('Should add an error if full name have an invalid space after words', () => {
+    const sut = new FullNameValidation()
+    const errors = sut.validate(invalidSpaceAfterFullName)
     expect(errors[0]).toBe('Full name must be separated by a single space')
   })
 })
