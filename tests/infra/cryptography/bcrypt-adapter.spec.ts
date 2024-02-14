2f2a41ddd1628fa7879aa9f475ee782475bc7a00
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 08:48:02 -0300

    test: ensure BcryptAdapter calls hash with correct values

---
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,29 @@
+import bcrypt from 'bcrypt'
+import { faker } from '@faker-js/faker'
+
+import { BcryptAdapter } from '@/infra/cryptography'
+
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return digest
+  }
+}))
+
+const salt: number = 12
+const plainText: string = faker.word.words()
+const digest: string = faker.word.words()
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('BcryptAdapter', () => {
+  describe('Hasher', () => {
+    test('Should call hash with correct values', async() => {
+      const sut = makeSut()
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await sut.hash(plainText)
+      expect(hashSpy).toHaveBeenCalledWith(plainText, salt)
+    })
+  })
+})
