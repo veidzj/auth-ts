7cab79ef7f49dbd9ebb75b72ceaf747d5ef8e6bc
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 15:08:28 -0300

    feat: ensure UsernameValidation returns an error if username is greater than 16 characters long

--- src/validation/validators/username-validation.ts
+++ src/validation/validators/username-validation.ts
@@ -4,8 +4,8 @@ export class UsernameValidation implements Validation<string> {
   private readonly errors: string[] = []
 
   validate(username: string): string[] {
-    if (username.length < 3) {
-      this.errors.push('Username must be at least 3 characters long')
+    if (username.length < 3 || username.length > 16) {
+      this.errors.push('Username must be between 3 and 16 characters long')
     }
     return this.errors
   }
