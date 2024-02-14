54c9f49308b1c0caed7b68c525288eb2c5ed1f09
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 14:50:00 -0300

    refactor: use faker

--- tests/validation/validators/username-validation.spec.ts
+++ tests/validation/validators/username-validation.spec.ts
@@ -1,9 +1,17 @@
+import { faker } from '@faker-js/faker'
+
 import { UsernameValidation } from '@/validation/validators'
 
 describe('UsernameValidation', () => {
+  let shortUsername: string
+
+  beforeAll(() => {
+    shortUsername = faker.string.alpha({ length: { min: 1, max: 2 } })
+  })
+
   test('Should return an error if username is less than 3 characters long', () => {
     const sut = new UsernameValidation()
-    const error = sut.validate('')
+    const error = sut.validate(shortUsername)
     expect(error).toEqual(new Error('Username must be at least 3 characters long'))
   })
 })
