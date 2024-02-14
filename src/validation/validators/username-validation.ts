921294b48d1472313ca21631ff6f71cfb035c44d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-14 15:13:59 -0300

    feat: ensure UsernameValidation returns an error if username starts with a number

--- src/validation/validators/username-validation.ts
+++ src/validation/validators/username-validation.ts
@@ -7,6 +7,9 @@ export class UsernameValidation implements Validation<string> {
     if (username.length < 3 || username.length > 16) {
       this.errors.push('Username must be between 3 and 16 characters long')
     }
+    if (!/^[a-zA-Z]/.test(username)) {
+      this.errors.push('Username must start with a letter')
+    }
     return this.errors
   }
 }
