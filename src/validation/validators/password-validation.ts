ddf341b3b59c844f3afd7284cddae3780dd9c2ae
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-15 10:38:46 -0300

    feat: ensure PasswordValidation adds an error if password contains username

--- src/validation/validators/password-validation.ts
+++ src/validation/validators/password-validation.ts
@@ -3,13 +3,19 @@ import { type Validation } from '@/presentation/protocols'
 export class PasswordValidation implements Validation<string> {
   private readonly errors: string[] = []
 
-  validate(password: string, options?: { username: string, fullName: string, email: string, birthdate: string }): string[] {
+  validate(password: string, options: { username: string, fullName: string, email: string, birthdate: string }): string[] {
     if (password.length < 6 || password.length > 255) {
       this.errors.push('Password must be between 6 and 255 characters long')
     }
     if (!/(?=.*[a-zA-Z])(?=.*\d)(?=.*[^a-zA-Z\d])/.test(password)) {
       this.errors.push('Password must contain at least 1 letter, 1 digit, and 1 special character')
     }
+    const personalData = [options.username, options.email, options.fullName, options.birthdate]
+    personalData.forEach((data: string) => {
+      if (password.toLowerCase().includes(data.toLowerCase())) {
+        this.errors.push('Password cannot contain personal data')
+      }
+    })
     return this.errors
   }
 }
