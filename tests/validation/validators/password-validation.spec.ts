26d2c5048c21c1fe2eb6c3d9e95a77293e508e71
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-15 10:21:40 -0300

    test: ensure PasswordValidation adds an error if password does not contains a letter

--- tests/validation/validators/password-validation.spec.ts
+++ tests/validation/validators/password-validation.spec.ts
@@ -5,10 +5,12 @@ import { PasswordValidation } from '@/validation/validators'
 describe('PasswordValidation', () => {
   let shortPassword: string
   let longPassword: string
+  let passwordWithNoLetter: string
 
   beforeAll(() => {
     shortPassword = faker.string.alpha({ length: { min: 1, max: 5 } })
     longPassword = faker.string.alpha(256)
+    passwordWithNoLetter = faker.string.numeric(6)
   })
 
   test('Should add an error if password is less than 6 characters long', () => {
@@ -21,5 +23,11 @@ describe('PasswordValidation', () => {
     const sut = new PasswordValidation()
     const errors = sut.validate(longPassword)
     expect(errors[0]).toBe('Password must be between 6 and 255 characters long')
+  })
+
+  test('Should add an error if password does not contains a letter', () => {
+    const sut = new PasswordValidation()
+    const errors = sut.validate(passwordWithNoLetter)
+    expect(errors[0]).toBe('Password must contain at least 1 letter, 1 digit, and 1 special character')
   })
 })
