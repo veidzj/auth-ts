cf48894a74c4c86a19787863cf820e2718d7ab82
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-15 20:55:12 -0300

    test: ensure BirthdateValidation does not adds an error if birthdate is in valid format

--- tests/validation/validators/birthdate-validation.spec.ts
+++ tests/validation/validators/birthdate-validation.spec.ts
@@ -4,10 +4,14 @@ import { BirthdateValidation } from '@/validation/validators'
 
 describe('BirthdateValidation', () => {
   let invalidBirthdate: { birthdate: string }
+  let validBirthdate: { birthdate: string }
 
   beforeAll(() => {
     invalidBirthdate = {
       birthdate: faker.date.anytime().toString()
+    }
+    validBirthdate = {
+      birthdate: `${faker.string.numeric(4)}-${faker.string.numeric(2)}-${faker.string.numeric(2)}`
     }
   })
 
@@ -15,5 +19,11 @@ describe('BirthdateValidation', () => {
     const sut = new BirthdateValidation()
     const errors = sut.validate(invalidBirthdate)
     expect(errors[0]).toBe('Birthdate must be in the format YYYY-MM-DD (ISO 8601)')
+  })
+
+  test('Should not add an error if birthdate is in valid format', () => {
+    const sut = new BirthdateValidation()
+    const errors = sut.validate(validBirthdate)
+    expect(errors).toEqual([])
   })
 })
