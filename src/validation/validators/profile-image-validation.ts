67047081decc47aa6769a821a6334156f891206a
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-15 20:16:42 -0300

    feat: ensure ProfileImageValidation does not adds an error if profile image is not provided

--- src/validation/validators/profile-image-validation.ts
+++ src/validation/validators/profile-image-validation.ts
@@ -5,9 +5,11 @@ import { type Validation } from '@/presentation/protocols'
 export class ProfileImageValidation implements Validation {
   private readonly errors: string[] = []
 
-  validate(input: { profileImage: string }): string[] {
-    if (!validator.isURL(input.profileImage)) {
-      this.errors.push('Profile image must be a valid url')
+  validate(input: { profileImage: string | undefined }): string[] {
+    if (input.profileImage) {
+      if (!validator.isURL(input.profileImage)) {
+        this.errors.push('Profile image must be a valid url')
+      }
     }
     return this.errors
   }
