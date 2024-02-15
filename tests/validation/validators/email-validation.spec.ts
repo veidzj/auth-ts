793bc678cecaf62b9bad6485deb7030955e99239
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-15 20:41:23 -0300

    test: ensure EmailValidation does not adds an error if email is valid

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -4,16 +4,26 @@ import { EmailValidation } from '@/validation/validators'
 
 describe('EmailValidation', () => {
   let invalidEmail: { email: string }
+  let validEmail: { email: string }
 
   beforeAll(() => {
     invalidEmail = {
       email: faker.string.alpha(12)
     }
+    validEmail = {
+      email: faker.internet.email()
+    }
   })
 
-  test('Should add an error if email is not a valid email', () => {
+  test('Should add an error if email is invalid', () => {
     const sut = new EmailValidation()
     const errors = sut.validate(invalidEmail)
     expect(errors[0]).toBe('Email must be a valid email')
+  })
+
+  test('Should not add an error if email is valid', () => {
+    const sut = new EmailValidation()
+    const errors = sut.validate(validEmail)
+    expect(errors).toEqual([])
   })
 })
