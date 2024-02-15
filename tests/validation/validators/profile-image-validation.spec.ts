7ed72e46d8a492ccc6a458cb33c77f340b64134c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-15 20:13:00 -0300

    test: ensure ProfileImageValidation adds an error if profile image is not a valid url

---
+++ tests/validation/validators/profile-image-validation.spec.ts
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker'
+
+import { ProfileImageValidation } from '@/validation/validators'
+
+describe('ProfileImageValidation', () => {
+  let invalidUrl: { profileImage: string }
+
+  beforeAll(() => {
+    invalidUrl = {
+      profileImage: faker.string.alpha(12)
+    }
+  })
+
+  test('Should add an error if profile image is not a valid url', () => {
+    const sut = new ProfileImageValidation()
+    const errors = sut.validate(invalidUrl)
+    expect(errors[0]).toBe('Profile image must be a valid url')
+  })
+})
