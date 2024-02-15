fc80dee9c5ce45ae97201111c3bfa78e3dcd7c7e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-15 20:16:52 -0300

    test: ensure ProfileImageValidation does not adds an error if profile image is not provided

--- tests/validation/validators/profile-image-validation.spec.ts
+++ tests/validation/validators/profile-image-validation.spec.ts
@@ -16,4 +16,10 @@ describe('ProfileImageValidation', () => {
     const errors = sut.validate(invalidUrl)
     expect(errors[0]).toBe('Profile image must be a valid url')
   })
+
+  test('Should not add an error if profile image is not provided', () => {
+    const sut = new ProfileImageValidation()
+    const errors = sut.validate({ profileImage: undefined })
+    expect(errors).toEqual([])
+  })
 })
