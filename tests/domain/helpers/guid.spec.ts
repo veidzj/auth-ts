85fe805e08ab4dbad51f0ed08e6aad992ebcb90f
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-15 19:35:52 -0300

    test: ensure generateGUID generates unique GUIDs

--- tests/domain/helpers/guid.spec.ts
+++ tests/domain/helpers/guid.spec.ts
@@ -7,4 +7,10 @@ describe('generateGUID', () => {
       /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
     )
   })
+
+  test('Should generate unique GUIDs', () => {
+    const sut1 = generateGUID()
+    const sut2 = generateGUID()
+    expect(sut1).not.toEqual(sut2)
+  })
 })
