22ca7f17ec0e3b5e949a16b44721b0c34c30f517
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-21 17:25:51 -0300

    refactor: move sut creation to a factory

--- tests/validation/validators/full-name-validation.spec.ts
+++ tests/validation/validators/full-name-validation.spec.ts
@@ -3,6 +3,10 @@ import { faker } from '@faker-js/faker'
 import { FullNameValidation } from '@/validation/validators'
 import { ValidationError } from '@/validation/errors'
 
+const makeSut = (): FullNameValidation => {
+  return new FullNameValidation()
+}
+
 describe('FullNameValidation', () => {
   let shortFullName: FullNameValidation.Input
   let longFullName: FullNameValidation.Input
@@ -41,56 +45,56 @@ describe('FullNameValidation', () => {
   })
 
   test('Should throw ValidationError if full name is less than 3 characters long', () => {
-    const sut = new FullNameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(shortFullName)
     }).toThrow(new ValidationError('Full name must be between 3 and 50 characters long'))
   })
 
   test('Should throw ValidationError if full name is greater than 50 characters long', () => {
-    const sut = new FullNameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(longFullName)
     }).toThrow(new ValidationError('Full name must be between 3 and 50 characters long'))
   })
 
   test('Should throw ValidationError if full name starts with a lowercase letter', () => {
-    const sut = new FullNameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(lowercaseFullName)
     }).toThrow(new ValidationError('Full name must start with an uppercase letter'))
   })
 
   test('Should throw ValidationError if full name have an invalid space between words', () => {
-    const sut = new FullNameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(invalidSpaceBetweenFullName)
     }).toThrow(new ValidationError('Full name must be separated by a single space'))
   })
 
   test('Should throw ValidationError if full name have an invalid space after words', () => {
-    const sut = new FullNameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(invalidSpaceAfterFullName)
     }).toThrow(new ValidationError('Full name must be separated by a single space'))
   })
 
   test('Should throw ValidationError if full name contains special character', () => {
-    const sut = new FullNameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(fullNameWithSpecialCharacter)
     }).toThrow(new ValidationError('Full name can only contain letters and letters with accents'))
   })
 
   test('Should throw ValidationError if full name contains an isolated accent', () => {
-    const sut = new FullNameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(fullNameWithIsolatedAccent)
     }).toThrow(new ValidationError('Full name can only contain letters and letters with accents'))
   })
 
   test('Should not throw if full name is valid', () => {
-    const sut = new FullNameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(validFullName)
     }).not.toThrow()
