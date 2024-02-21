e652703e1e6fca23c247655f7416d8cb774cdb32
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-21 17:27:17 -0300

    refactor: move sut creation to a factory

--- tests/validation/validators/username-validation.spec.ts
+++ tests/validation/validators/username-validation.spec.ts
@@ -3,6 +3,10 @@ import { faker } from '@faker-js/faker'
 import { UsernameValidation } from '@/validation/validators'
 import { ValidationError } from '@/validation/errors'
 
+const makeSut = (): UsernameValidation => {
+  return new UsernameValidation()
+}
+
 describe('UsernameValidation', () => {
   let shortUsername: UsernameValidation.Input
   let longUsername: UsernameValidation.Input
@@ -33,42 +37,42 @@ describe('UsernameValidation', () => {
   })
 
   test('Should throw ValidationError if username is less than 3 characters long', () => {
-    const sut = new UsernameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(shortUsername)
     }).toThrow(new ValidationError('Username must be between 3 and 16 characters long'))
   })
 
   test('Should throw ValidationError if username is greater than 16 characters long', () => {
-    const sut = new UsernameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(longUsername)
     }).toThrow(new ValidationError('Username must be between 3 and 16 characters long'))
   })
 
   test('Should throw ValidationError if username starts with a number', () => {
-    const sut = new UsernameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(usernameStartsWithNumber)
     }).toThrow(new ValidationError('Username must start with a letter'))
   })
 
   test('Should throw ValidationError if username contains a special character', () => {
-    const sut = new UsernameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(usernameWithSpecialCharacter)
     }).toThrow(new ValidationError('Username can only contain letters, digits, underscore, hyphen and dot'))
   })
 
   test('Should throw ValidationError if username contains spaces', () => {
-    const sut = new UsernameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(usernameWithSpaces)
     }).toThrow(new ValidationError('Username must not contain spaces'))
   })
 
   test('Should return null if username is valid', () => {
-    const sut = new UsernameValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(validUsername)
     }).not.toThrow()
