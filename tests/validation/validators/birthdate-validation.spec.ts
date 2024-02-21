360c512b2165086a627cb8f914de0fe29f2a7508
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-21 17:24:42 -0300

    refactor: move sut creation to a factory

--- tests/validation/validators/birthdate-validation.spec.ts
+++ tests/validation/validators/birthdate-validation.spec.ts
@@ -3,6 +3,10 @@ import { faker } from '@faker-js/faker'
 import { BirthdateValidation } from '@/validation/validators'
 import { ValidationError } from '@/validation/errors'
 
+const makeSut = (): BirthdateValidation => {
+  return new BirthdateValidation()
+}
+
 describe('BirthdateValidation', () => {
   let invalidBirthdate: BirthdateValidation.Input
   let validBirthdate: BirthdateValidation.Input
@@ -17,14 +21,14 @@ describe('BirthdateValidation', () => {
   })
 
   test('Should throw ValidationError if birthdate is in invalid format', () => {
-    const sut = new BirthdateValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(invalidBirthdate)
     }).toThrow(new ValidationError('Birthdate must be in the format YYYY-MM-DD (ISO 8601)'))
   })
 
   test('Should not throw if birthdate is in valid format', () => {
-    const sut = new BirthdateValidation()
+    const sut = makeSut()
     expect(() => {
       sut.validate(validBirthdate)
     }).not.toThrow()
