4c0dc48d442af4a99483591bd65933a481e10e35
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-22 19:50:05 -0300

    refactor: move sut creation to a factory

--- tests/presentation/controllers/queries/sign-in-controller.spec.ts
+++ tests/presentation/controllers/queries/sign-in-controller.spec.ts
@@ -3,6 +3,20 @@ import { faker } from '@faker-js/faker'
 import { ValidationSpy } from '@/tests/presentation/mocks'
 import { SignInController } from '@/presentation/controllers/queries'
 
+interface Sut {
+  sut: SignInController
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): Sut => {
+  const validationSpy = new ValidationSpy()
+  const sut = new SignInController(validationSpy)
+  return {
+    sut,
+    validationSpy
+  }
+}
+
 const mockRequest = (): SignInController.Request => ({
   email: faker.internet.email(),
   password: faker.internet.password()
@@ -10,8 +24,7 @@ const mockRequest = (): SignInController.Request => ({
 
 describe('SignInController', () => {
   test('Should call Validation with correct values', async() => {
-    const validationSpy = new ValidationSpy()
-    const sut = new SignInController(validationSpy)
+    const { sut, validationSpy } = makeSut()
     const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
