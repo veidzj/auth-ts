a21a600dedd08f312e1302aa8979bf2ddaeceffd
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-22 19:51:41 -0300

    test: ensure SignInController returns badRequest if Validation throws ValidationError

--- tests/presentation/controllers/queries/sign-in-controller.spec.ts
+++ tests/presentation/controllers/queries/sign-in-controller.spec.ts
@@ -2,6 +2,8 @@ import { faker } from '@faker-js/faker'
 
 import { ValidationSpy } from '@/tests/presentation/mocks'
 import { SignInController } from '@/presentation/controllers/queries'
+import { HttpHelper } from '@/presentation/helpers'
+import { ValidationError } from '@/validation/errors'
 
 interface Sut {
   sut: SignInController
@@ -28,5 +30,13 @@ describe('SignInController', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
+  })
+
+  test('Should return badRequest if Validation throws ValidationError', async() => {
+    const { sut, validationSpy } = makeSut()
+    const errorMessage = faker.word.words()
+    jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new ValidationError(errorMessage) })
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(HttpHelper.badRequest(new ValidationError(errorMessage)))
   })
 })
