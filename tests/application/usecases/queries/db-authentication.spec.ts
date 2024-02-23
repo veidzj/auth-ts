e7067cff12741b3351d88329f4d90a895dd46c1d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-23 20:21:47 -0300

    test: ensure DbAuthentication throws AccountNotFoundError if GetAccountByEmailRepository returns null

--- tests/application/usecases/queries/db-authentication.spec.ts
+++ tests/application/usecases/queries/db-authentication.spec.ts
@@ -1,8 +1,9 @@
 import { faker } from '@faker-js/faker'
 
+import { GetAccountByEmailRepositorySpy } from '@/tests/application/mocks/queries'
 import { DbAuthentication } from '@/application/usecases/queries'
 import { type Authentication } from '@/domain/usecases/queries'
-import { GetAccountByEmailRepositorySpy } from '@/tests/application/mocks/queries'
+import { AccountNotFoundError } from '@/domain/errors'
 
 interface Sut {
   sut: DbAuthentication
@@ -30,6 +31,13 @@ describe('DbAuthentication', () => {
       const authenticationInput = mockAuthenticationInput()
       await sut.auth(authenticationInput)
       expect(getAccountByEmailRepositorySpy.email).toBe(authenticationInput.email)
+    })
+
+    test('Should throw AccountNotFoundError if GetAccountByEmailRepository returns null', async() => {
+      const { sut, getAccountByEmailRepositorySpy } = makeSut()
+      getAccountByEmailRepositorySpy.output = null
+      const promise = sut.auth(mockAuthenticationInput())
+      await expect(promise).rejects.toThrow(new AccountNotFoundError())
     })
 
     test('Should throw if GetAccountByEmailRepository throws', async() => {
