b45262a8933e20e198a0168b7dafb46fd361447a
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-23 20:35:37 -0300

    test: ensure DbAuthentication throws if Encrypter throws

--- tests/application/usecases/queries/db-authentication.spec.ts
+++ tests/application/usecases/queries/db-authentication.spec.ts
@@ -85,5 +85,12 @@ describe('DbAuthentication', () => {
       await sut.auth(mockAuthenticationInput())
       expect(encrypterSpy.plainText).toBe(getAccountByEmailRepositorySpy.output?.id)
     })
+
+    test('Should throw if Encrypter throws', async() => {
+      const { sut, encrypterSpy } = makeSut()
+      jest.spyOn(encrypterSpy, 'encrypt').mockRejectedValueOnce(new Error())
+      const promise = sut.auth(mockAuthenticationInput())
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
