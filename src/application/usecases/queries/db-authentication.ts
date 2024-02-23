ff1648a8c97e52ad29c8088f4e7ed048b0578249
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-23 20:27:06 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/application/usecases/queries/db-authentication.ts
+++ src/application/usecases/queries/db-authentication.ts
@@ -1,15 +1,20 @@
-import { type Authentication } from '@/domain/usecases/queries'
 import { type GetAccountByEmailRepository } from '@/application/protocols/queries'
+import { type HashComparer } from '@/application/protocols/cryptography'
+import { type Authentication } from '@/domain/usecases/queries'
 import { AccountNotFoundError } from '@/domain/errors'
 
 export class DbAuthentication implements Authentication {
-  constructor(private readonly getAccountByEmailRepository: GetAccountByEmailRepository) {}
+  constructor(
+    private readonly getAccountByEmailRepository: GetAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
+  ) {}
 
   public async auth(input: Authentication.Input): Promise<Authentication.Output> {
     const account = await this.getAccountByEmailRepository.get(input.email)
     if (!account) {
       throw new AccountNotFoundError()
     }
+    await this.hashComparer.compare(input.password, account.password)
     return {
       accessToken: ''
     }
