56a1f72eddf4edd994f10a36b949cab1d77acbe2
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-24 12:40:09 -0300

    test: ensure UpdateAccessTokenMongoRepository throws if mongo throws

--- tests/infra/db/mongodb/commands/update-access-token-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/commands/update-access-token-mongo-repository.spec.ts
@@ -1,15 +1,21 @@
-import { type Collection } from 'mongodb'
+import { Collection } from 'mongodb'
 import { faker } from '@faker-js/faker'
 
 import { connectToDatabase, disconnectFromDatabase, clearCollection, getCollection } from '@/tests/infra/db/mongodb'
 import { mockAddAccountRepositoryInput } from '@/tests/application/mocks/inputs'
 import { UpdateAccessTokenMongoRepository } from '@/infra/db/mongodb/commands'
+import { type UpdateAccessTokenRepository } from '@/application/protocols/commands'
 
 let accountCollection: Collection
 
 const makeSut = (): UpdateAccessTokenMongoRepository => {
   return new UpdateAccessTokenMongoRepository()
 }
+
+const mockUpdateAccessTokenInput = (): UpdateAccessTokenRepository.Input => ({
+  id: faker.string.uuid(),
+  accessToken: faker.string.uuid()
+})
 
 describe('UpdateAccessTokenMongoRepository', () => {
   beforeAll(async() => {
@@ -35,5 +41,12 @@ describe('UpdateAccessTokenMongoRepository', () => {
     const account = await accountCollection.findOne({ id: fakeAccount?.id })
     expect(account).toBeTruthy()
     expect(account?.accessToken).toBe(accessToken)
+  })
+
+  test('Should throw if mongo throws', async() => {
+    const sut = makeSut()
+    jest.spyOn(Collection.prototype, 'updateOne').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.update(mockUpdateAccessTokenInput())
+    await expect(promise).rejects.toThrow()
   })
 })
