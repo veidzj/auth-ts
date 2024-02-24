e53c8e5428c73fc68e1039dbd3eb9b89f04c6291
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-24 11:47:57 -0300

    test: ensure JwtAdapter calls sign with correct values

---
+++ tests/infra/cryptography/jwt-adapter.spec.ts
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken'
+
+import { faker } from '@faker-js/faker'
+import { JwtAdapter } from '@/infra/cryptography'
+
+jest.mock('jsonwebtoken', () => ({
+  sign(): string {
+    return jwtToken
+  }
+}))
+
+const jwtSecret: string = faker.string.uuid()
+const plainText: string = faker.string.uuid()
+const jwtToken: string = faker.string.uuid()
+
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter(jwtSecret)
+}
+
+describe('JwtAdapter', () => {
+  describe('Encrypter', () => {
+    test('Should call sign with correct values', async() => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt(plainText)
+      expect(signSpy).toHaveBeenCalledWith({ id: plainText }, jwtSecret, { expiresIn: '1h' })
+    })
+  })
+})
