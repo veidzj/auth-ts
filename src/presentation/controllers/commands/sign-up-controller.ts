c466c74bd88c9867dd3268d88daab84c9a45bc45
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-24 13:41:50 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/presentation/controllers/commands/sign-up-controller.ts
+++ src/presentation/controllers/commands/sign-up-controller.ts
@@ -1,18 +1,21 @@
 import { type Validation, type Controller, type HttpResponse } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type AddAccount } from '@/domain/usecases/commands'
+import { type Authentication } from '@/domain/usecases/queries'
 import { ValidationError } from '@/validation/errors'
 
 export class SignUpController implements Controller {
   constructor(
     private readonly validation: Validation,
-    private readonly addAccount: AddAccount
+    private readonly addAccount: AddAccount,
+    private readonly authentication: Authentication
   ) {}
 
   public async handle(request: SignUpController.Request): Promise<HttpResponse> {
     try {
       this.validation.validate(request)
       await this.addAccount.add(request)
+      await this.authentication.auth({ email: request.email, password: request.password })
       return HttpHelper.ok({})
     } catch (error) {
       if (error instanceof ValidationError) {
