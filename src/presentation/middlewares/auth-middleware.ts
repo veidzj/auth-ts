195634dda01de61ad5d3a69bb6845f8144dfeddc
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-28 13:34:36 -0300

    feat: ensure AuthMiddleware returns ok on success

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -14,8 +14,8 @@ export class AuthMiddleware implements Middleware {
       if (!request.accessToken) {
         return HttpHelper.unauthorized(new InvalidCredentialsError())
       }
-      await this.getAccountIdByToken.get({ accessToken: request.accessToken, role: this.role })
-      return HttpHelper.ok({})
+      const accountId = await this.getAccountIdByToken.get({ accessToken: request.accessToken, role: this.role })
+      return HttpHelper.ok(accountId)
     } catch (error) {
       if (error instanceof InvalidCredentialsError) {
         return HttpHelper.unauthorized(error)
