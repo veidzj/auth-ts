14742eaf06a842213707c6bab598849443e2d201
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-01 01:18:33 -0300

    test: ensure JwtAdapter calls verify with correct values

--- tests/infra/cryptography/jwt-adapter.spec.ts
+++ tests/infra/cryptography/jwt-adapter.spec.ts
@@ -6,12 +6,17 @@ import { JwtAdapter } from '@/infra/cryptography'
 jest.mock('jsonwebtoken', () => ({
   sign(): string {
     return jwtToken
+  },
+
+  verify(): string {
+    return jwtDecodedToken
   }
 }))
 
 const jwtSecret: string = faker.string.uuid()
 const plainText: string = faker.string.uuid()
 const jwtToken: string = faker.string.uuid()
+const jwtDecodedToken: string = faker.string.uuid()
 
 const makeSut = (): JwtAdapter => {
   return new JwtAdapter(jwtSecret)
@@ -37,6 +42,15 @@ describe('JwtAdapter', () => {
       const sut = makeSut()
       const accessToken = await sut.encrypt(plainText)
       expect(accessToken).toBe(jwtToken)
+    })
+  })
+
+  describe('Decrypter', () => {
+    test('Should call verify with correct values', async() => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt(jwtToken)
+      expect(verifySpy).toHaveBeenCalledWith(jwtToken, jwtSecret)
     })
   })
 })
