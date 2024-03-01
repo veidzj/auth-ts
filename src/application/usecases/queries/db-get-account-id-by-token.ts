978e3e7decbbdbf014a83edc57ad481fbf66c9db
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-29 22:55:37 -0300

    feat: ensure DbGetAccountIdByToken calls Decrypter with correct value

--- src/application/usecases/queries/db-get-account-id-by-token.ts
+++ src/application/usecases/queries/db-get-account-id-by-token.ts
@@ -1,11 +1,16 @@
+import { type Decrypter } from '@/application/protocols/cryptography'
 import { type GetAccountIdByTokenRepository } from '@/application/protocols/queries'
 import { AccessDeniedError } from '@/domain/errors'
 import { type GetAccountIdByToken } from '@/domain/usecases/queries'
 
 export class DbGetAccountIdByToken implements GetAccountIdByToken {
-  constructor(private readonly getAccountIdByTokenRepository: GetAccountIdByTokenRepository) {}
+  constructor(
+    private readonly decrypter: Decrypter,
+    private readonly getAccountIdByTokenRepository: GetAccountIdByTokenRepository
+  ) {}
 
   public async get(input: GetAccountIdByToken.Input): Promise<GetAccountIdByToken.Output> {
+    await this.decrypter.decrypt(input.accessToken)
     const accountId = await this.getAccountIdByTokenRepository.get(input)
     if (!accountId) {
       throw new AccessDeniedError()
