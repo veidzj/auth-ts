472342a3e3e53e6e1c8dc9620b02937949f7f083
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-29 22:51:24 -0300

    feat: ensure DbGetAccountIdByToken returns accountId on success

--- src/application/usecases/queries/db-get-account-id-by-token.ts
+++ src/application/usecases/queries/db-get-account-id-by-token.ts
@@ -10,8 +10,6 @@ export class DbGetAccountIdByToken implements GetAccountIdByToken {
     if (!accountId) {
       throw new AccessDeniedError()
     }
-    return {
-      accountId: ''
-    }
+    return accountId
   }
 }
