2a85e6ad0deca28bc13d770ac90b082d1c7fbe71
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-01 01:43:07 -0300

    test: ensure GetAccountIdByTokenMongoRepository returns null if there is no account with the given accessToken

--- tests/infra/db/mongodb/queries/get-account-id-by-token-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/queries/get-account-id-by-token-mongo-repository.spec.ts
@@ -3,6 +3,7 @@ import { type Collection } from 'mongodb'
 import { connectToDatabase, disconnectFromDatabase, clearCollection, getCollection } from '@/tests/infra/db/mongodb'
 import { mockAddAccountRepositoryInput } from '@/tests/application/mocks/inputs'
 import { GetAccountIdByTokenMongoRepository } from '@/infra/db/mongodb/queries'
+import { faker } from '@faker-js/faker'
 
 let accountCollection: Collection
 
@@ -26,12 +27,23 @@ describe('GetAccountIdByTokenMongoRepository', () => {
 
   test('Should return an accountId on success', async() => {
     const sut = makeSut()
-    const addAccountRepositoryInput = { ...mockAddAccountRepositoryInput(), accessToken: 'teste' }
+    const addAccountRepositoryInput = { ...mockAddAccountRepositoryInput(), accessToken: faker.word.words() }
     await accountCollection.insertOne(addAccountRepositoryInput)
     const account = await sut.get({
       accessToken: addAccountRepositoryInput.accessToken,
       role: addAccountRepositoryInput.roles[0]
     })
     expect(account?.accountId).toBe(addAccountRepositoryInput.id)
+  })
+
+  test('Should return null if there is no account with the given accessToken', async() => {
+    const sut = makeSut()
+    const addAccountRepositoryInput = { ...mockAddAccountRepositoryInput(), accessToken: faker.word.words() }
+    await accountCollection.insertOne(addAccountRepositoryInput)
+    const account = await sut.get({
+      accessToken: faker.word.words(),
+      role: addAccountRepositoryInput.roles[0]
+    })
+    expect(account).toBeNull()
   })
 })
