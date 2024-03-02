b9b66c3291b36429b3b9527489a0aee7ae7a7cc4
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-01 22:36:28 -0300

    feat: ensure DeactivateAccountController returns notFound if DeactivateAccount throws AccountNotFoundError

--- src/presentation/controllers/commands/deactivate-account-controller.ts
+++ src/presentation/controllers/commands/deactivate-account-controller.ts
@@ -1,10 +1,21 @@
+import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { HttpHelper } from '@/presentation/helpers'
 import { type DeactivateAccount } from '@/domain/usecases/commands'
+import { AccountNotFoundError } from '@/domain/errors'
 
-export class DeactivateAccountController {
+export class DeactivateAccountController implements Controller {
   constructor(private readonly deactivateAccount: DeactivateAccount) {}
 
-  public async handle(request: DeactivateAccountController.Request): Promise<void> {
-    await this.deactivateAccount.deactivate(request)
+  public async handle(request: DeactivateAccountController.Request): Promise<HttpResponse> {
+    try {
+      await this.deactivateAccount.deactivate(request)
+      return HttpHelper.ok({})
+    } catch (error) {
+      if (error instanceof AccountNotFoundError) {
+        return HttpHelper.notFound(error)
+      }
+      return HttpHelper.serverError()
+    }
   }
 }
 
