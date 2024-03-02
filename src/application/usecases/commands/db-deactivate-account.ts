5bceab3259748706bfd9ca6f302dc4ae1830f8d6
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-02 18:07:34 -0300

    feat: ensure DbDeactivateAccount throws AccountNotFoundError if CheckAccountByIdRepository returns false

--- src/application/usecases/commands/db-deactivate-account.ts
+++ src/application/usecases/commands/db-deactivate-account.ts
@@ -1,6 +1,7 @@
 import { type DeactivateAccountRepository } from '@/application/protocols/commands'
 import { type CheckAccountByIdRepository } from '@/application/protocols/queries'
 import { type DeactivateAccount } from '@/domain/usecases/commands'
+import { AccountNotFoundError } from '@/domain/errors'
 
 export class DbDeactivateAccount implements DeactivateAccount {
   constructor(
@@ -9,7 +10,10 @@ export class DbDeactivateAccount implements DeactivateAccount {
   ) {}
 
   public async deactivate(input: DeactivateAccount.Input): Promise<void> {
-    await this.checkAccountByIdRepository.check(input.accountId)
+    const account = await this.checkAccountByIdRepository.check(input.accountId)
+    if (!account) {
+      throw new AccountNotFoundError()
+    }
     await this.deactivateAccountRepository.deactivate(input)
   }
 }
