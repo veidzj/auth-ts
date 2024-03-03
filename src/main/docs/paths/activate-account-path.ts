05521682f024eac5a0d66e80fb7fce476bb95ee4
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-03 20:03:33 -0300

    docs: add activate account route swagger docs

---
+++ src/main/docs/paths/activate-account-path.ts
@@ -0,0 +1,37 @@
+export const activateAccountPath = {
+  put: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Account'],
+    summary: 'Activates an account',
+    parameters: [{
+      in: 'path',
+      name: 'accountId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        $ref: '#/schemas/activateAccountOutput'
+      },
+      401: {
+        $ref: '#/components/unauthorized'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      409: {
+        $ref: '#/components/conflict'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
--- src/main/docs/paths/index.ts
+++ src/main/docs/paths/index.ts
@@ -1,3 +1,4 @@
+export * from './activate-account-path'
 export * from './deactivate-account-path'
 export * from './sign-in-path'
 export * from './sign-up-path'
