0ceabf7bd68b883b6c90f5e9c52c7f98e4abd042
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-03 16:36:28 -0300

    test: ensure EmailValidation throws ValidationError if EmailValidator returns false

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker'
 
 import { EmailValidatorSpy } from '@/tests/validation/mocks'
 import { EmailValidation } from '@/validation/validators'
+import { ValidationError } from '@/validation/errors'
 
 interface Sut {
   sut: EmailValidation
@@ -23,5 +24,13 @@ describe('EmailValidation', () => {
     const email = faker.internet.email()
     sut.validate({ email })
     expect(emailValidatorSpy.email).toBe(email)
+  })
+
+  test('Should throw ValidationError if EmailValidator returns false', () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    emailValidatorSpy.isEmailValid = false
+    expect(() => {
+      sut.validate({ email: faker.internet.email() })
+    }).toThrow(new ValidationError('Email must be a valid email'))
   })
 })
