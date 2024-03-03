e5d117064ae9bca4ffd7be62a2c437cc2bf5c55a
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-02 22:54:27 -0300

    feat: ensure ActivateAccountController returns notFound if ActivateAccount throws AccountNotFoundError

--- src/presentation/controllers/commands/activate-account-controller.ts
+++ src/presentation/controllers/commands/activate-account-controller.ts
@@ -1,6 +1,7 @@
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type ActivateAccount } from '@/domain/usecases/commands'
+import { AccountNotFoundError } from '@/domain/errors'
 
 export class ActivateAccountController implements Controller {
   constructor(private readonly activateAccount: ActivateAccount) {}
@@ -10,6 +11,9 @@ export class ActivateAccountController implements Controller {
       await this.activateAccount.activate(request.accountId)
       return HttpHelper.ok({ message: 'Account successfully activated' })
     } catch (error) {
+      if (error instanceof AccountNotFoundError) {
+        return HttpHelper.notFound(error)
+      }
       return HttpHelper.serverError()
     }
   }
