c1d0c74717393bdb6f6163ea01de163212589bef
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-03 19:24:38 -0300

    test: ensure ProfileImageValidation throws ValidationError if URLValidator returns false

--- tests/validation/validators/profile-image-validation.spec.ts
+++ tests/validation/validators/profile-image-validation.spec.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker'
 
 import { URLValidatorSpy } from '@/tests/validation/mocks'
 import { ProfileImageValidation } from '@/validation/validators'
+import { ValidationError } from '@/validation/errors'
 
 interface Sut {
   sut: ProfileImageValidation
@@ -23,5 +24,13 @@ describe('ProfileImageValidation', () => {
     const url = faker.internet.url()
     sut.validate({ profileImage: url })
     expect(urlValidatorSpy.url).toBe(url)
+  })
+
+  test('Should throw ValidationError if URLValidator returns false', () => {
+    const { sut, urlValidatorSpy } = makeSut()
+    urlValidatorSpy.islURLValid = false
+    expect(() => {
+      sut.validate({ profileImage: faker.internet.url() })
+    }).toThrow(new ValidationError('Profile image must be a valid url'))
   })
 })
