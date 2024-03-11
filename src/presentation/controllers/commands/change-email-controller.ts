af527ea888c0dec268995aa44278c15995770d27
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-11 14:48:13 -0300

    feat: ensure ChangeEmailController calls Validation with correct values

--- src/presentation/controllers/commands/change-email-controller.ts
+++ src/presentation/controllers/commands/change-email-controller.ts
@@ -1,13 +1,17 @@
-import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { type Controller, type HttpResponse, type Validation } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type ChangeEmail } from '@/domain/usecases/commands'
 import { AccountNotFoundError, AccountAlreadyExistsError } from '@/domain/errors'
 
 export class ChangeEmailController implements Controller {
-  constructor(private readonly changeEmail: ChangeEmail) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly changeEmail: ChangeEmail
+  ) {}
 
   public async handle(request: ChangeEmailController.Request): Promise<HttpResponse> {
     try {
+      this.validation.validate(request)
       await this.changeEmail.change(request.currentEmail, request.newEmail)
       return HttpHelper.ok({ message: 'Email successfully changed' })
     } catch (error) {
