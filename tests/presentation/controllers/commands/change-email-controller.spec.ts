8f60fc0922c14ceb032e91c396565ef18991ca22
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-11 14:48:37 -0300

    test: ensure ChangeEmailController calls Validation with correct values

--- tests/presentation/controllers/commands/change-email-controller.spec.ts
+++ tests/presentation/controllers/commands/change-email-controller.spec.ts
@@ -4,17 +4,21 @@ import { ChangeEmailSpy } from '@/tests/domain/mocks/commands'
 import { ChangeEmailController } from '@/presentation/controllers/commands'
 import { HttpHelper } from '@/presentation/helpers'
 import { AccountNotFoundError, AccountAlreadyExistsError } from '@/domain/errors'
+import { ValidationSpy } from '@/tests/presentation/mocks'
 
 interface Sut {
   sut: ChangeEmailController
+  validationSpy: ValidationSpy
   changeEmailSpy: ChangeEmailSpy
 }
 
 const makeSut = (): Sut => {
+  const validationSpy = new ValidationSpy()
   const changeEmailSpy = new ChangeEmailSpy()
-  const sut = new ChangeEmailController(changeEmailSpy)
+  const sut = new ChangeEmailController(validationSpy, changeEmailSpy)
   return {
     sut,
+    validationSpy,
     changeEmailSpy
   }
 }
@@ -25,38 +29,49 @@ const mockRequest = (): ChangeEmailController.Request => ({
 })
 
 describe('ChangeEmailController', () => {
-  test('Should call ChangeEmail with correct values', async() => {
-    const { sut, changeEmailSpy } = makeSut()
-    const request = mockRequest()
-    await sut.handle(request)
-    expect(changeEmailSpy.currentEmail).toBe(request.currentEmail)
-    expect(changeEmailSpy.newEmail).toBe(request.newEmail)
+  describe('Validation', () => {
+    test('Should call Validation with correct values', async() => {
+      const { sut, validationSpy } = makeSut()
+      const request = mockRequest()
+      await sut.handle(request)
+      expect(validationSpy.input).toEqual(request)
+    })
   })
 
-  test('Should return notFound if ChangeEmail throws AccountNotFoundError', async() => {
-    const { sut, changeEmailSpy } = makeSut()
-    jest.spyOn(changeEmailSpy, 'change').mockRejectedValueOnce(new AccountNotFoundError())
-    const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(HttpHelper.notFound(new AccountNotFoundError()))
-  })
+  describe('ChangeEmail', () => {
+    test('Should call ChangeEmail with correct values', async() => {
+      const { sut, changeEmailSpy } = makeSut()
+      const request = mockRequest()
+      await sut.handle(request)
+      expect(changeEmailSpy.currentEmail).toBe(request.currentEmail)
+      expect(changeEmailSpy.newEmail).toBe(request.newEmail)
+    })
 
-  test('Should return conflict if ChangeEmail throws AccountAlreadyExistsError', async() => {
-    const { sut, changeEmailSpy } = makeSut()
-    jest.spyOn(changeEmailSpy, 'change').mockRejectedValueOnce(new AccountAlreadyExistsError())
-    const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(HttpHelper.conflict(new AccountAlreadyExistsError()))
-  })
+    test('Should return notFound if ChangeEmail throws AccountNotFoundError', async() => {
+      const { sut, changeEmailSpy } = makeSut()
+      jest.spyOn(changeEmailSpy, 'change').mockRejectedValueOnce(new AccountNotFoundError())
+      const httpResponse = await sut.handle(mockRequest())
+      expect(httpResponse).toEqual(HttpHelper.notFound(new AccountNotFoundError()))
+    })
 
-  test('Should return serverError if ChangeEmail throws', async() => {
-    const { sut, changeEmailSpy } = makeSut()
-    jest.spyOn(changeEmailSpy, 'change').mockRejectedValueOnce(new Error())
-    const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(HttpHelper.serverError())
-  })
+    test('Should return conflict if ChangeEmail throws AccountAlreadyExistsError', async() => {
+      const { sut, changeEmailSpy } = makeSut()
+      jest.spyOn(changeEmailSpy, 'change').mockRejectedValueOnce(new AccountAlreadyExistsError())
+      const httpResponse = await sut.handle(mockRequest())
+      expect(httpResponse).toEqual(HttpHelper.conflict(new AccountAlreadyExistsError()))
+    })
 
-  test('Should return ok on success', async() => {
-    const { sut } = makeSut()
-    const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(HttpHelper.ok({ message: 'Email successfully changed' }))
+    test('Should return serverError if ChangeEmail throws', async() => {
+      const { sut, changeEmailSpy } = makeSut()
+      jest.spyOn(changeEmailSpy, 'change').mockRejectedValueOnce(new Error())
+      const httpResponse = await sut.handle(mockRequest())
+      expect(httpResponse).toEqual(HttpHelper.serverError())
+    })
+
+    test('Should return ok on success', async() => {
+      const { sut } = makeSut()
+      const httpResponse = await sut.handle(mockRequest())
+      expect(httpResponse).toEqual(HttpHelper.ok({ message: 'Email successfully changed' }))
+    })
   })
 })
