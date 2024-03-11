7b098b58bffb4dd26e9734494e12f69effc33196
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-11 00:39:35 -0300

    feat: ensure ChangeEmailController returns notFound if ChangeEmail throws AccountNotFound

--- src/presentation/controllers/commands/change-email-controller.ts
+++ src/presentation/controllers/commands/change-email-controller.ts
@@ -1,13 +1,21 @@
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type ChangeEmail } from '@/domain/usecases/commands'
+import { AccountNotFoundError } from '@/domain/errors'
 
 export class ChangeEmailController implements Controller {
   constructor(private readonly changeEmail: ChangeEmail) {}
 
   public async handle(request: ChangeEmailController.Request): Promise<HttpResponse> {
-    await this.changeEmail.change(request.currentEmail, request.newEmail)
-    return HttpHelper.ok({})
+    try {
+      await this.changeEmail.change(request.currentEmail, request.newEmail)
+      return HttpHelper.ok({})
+    } catch (error) {
+      if (error instanceof AccountNotFoundError) {
+        return HttpHelper.notFound(error)
+      }
+      return HttpHelper.serverError()
+    }
   }
 }
 
