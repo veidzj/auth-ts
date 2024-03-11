99454b5cb0fe0027089dd88c21a954f1efffd09f
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-11 17:30:09 -0300

    feat: ensure DbChangeEmail throws AccountNotFoundError if first CheckAccountByEmailRepository returns false

--- src/application/usecases/commands/db-change-email.ts
+++ src/application/usecases/commands/db-change-email.ts
@@ -1,11 +1,15 @@
 import { type CheckAccountByEmailRepository } from '@/application/protocols/queries'
 import { type ChangeEmail } from '@/domain/usecases/commands'
+import { AccountNotFoundError } from '@/domain/errors'
 
 export class DbChangeEmail implements ChangeEmail {
   constructor(private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository) {}
 
   public async change(currentEmail: string, newEmail: string): Promise<void> {
-    await this.checkAccountByEmailRepository.check(currentEmail)
+    const accountWithCurrentEmailExists = await this.checkAccountByEmailRepository.check(currentEmail)
+    if (!accountWithCurrentEmailExists) {
+      throw new AccountNotFoundError()
+    }
     await this.checkAccountByEmailRepository.check(newEmail)
   }
 }
