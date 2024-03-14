756c3ad1f008c8b0f1cace80f1afe56db1dea5b2
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-14 00:32:27 -0300

    feat: ensure SendConfirmationCodeController returns badRequest if Validation throws ValidationError

--- src/presentation/controllers/commands/send-confirmation-code-controller.ts
+++ src/presentation/controllers/commands/send-confirmation-code-controller.ts
@@ -1,11 +1,19 @@
 import { type Controller, type HttpResponse, type Validation } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
+import { ValidationError } from '@/validation/errors'
 
 export class SendConfirmationCodeController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   public async handle(request: object): Promise<HttpResponse> {
-    this.validation.validate(request)
-    return HttpHelper.ok({})
+    try {
+      this.validation.validate(request)
+      return HttpHelper.ok({})
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return HttpHelper.badRequest(error)
+      }
+      return HttpHelper.serverError()
+    }
   }
 }
