b4073337d6335e4c52480c8c8cc71a3b67f34124
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-14 00:32:46 -0300

    test: ensure SendConfirmationCodeController returns badRequest if Validation throws ValidationError

--- tests/presentation/controllers/commands/send-confirmation-code-controller.spec.ts
+++ tests/presentation/controllers/commands/send-confirmation-code-controller.spec.ts
@@ -1,6 +1,9 @@
-import { SendConfirmationCodeController } from '@/presentation/controllers/commands'
-import { ValidationSpy } from '@/tests/presentation/mocks'
 import { faker } from '@faker-js/faker'
+
+import { ValidationSpy } from '@/tests/presentation/mocks'
+import { SendConfirmationCodeController } from '@/presentation/controllers/commands'
+import { HttpHelper } from '@/presentation/helpers'
+import { ValidationError } from '@/validation/errors'
 
 const mockRequest = (): object => ({
   email: faker.internet.email()
@@ -13,5 +16,14 @@ describe('SendConfirmationCodeController', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
+  })
+
+  test('Should return badRequest if Validation throws ValidationError', async() => {
+    const validationSpy = new ValidationSpy()
+    const sut = new SendConfirmationCodeController(validationSpy)
+    const errorMessage = faker.word.words()
+    jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new ValidationError(errorMessage) })
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(HttpHelper.badRequest(new ValidationError(errorMessage)))
   })
 })
