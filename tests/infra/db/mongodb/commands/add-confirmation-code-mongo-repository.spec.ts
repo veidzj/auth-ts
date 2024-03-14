4037436c46c5876cc8cd2ea2dc576dfb1f1324c9
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-14 18:59:18 -0300

    test: ensure AddConfirmationCodeMongoRepository adds a confirmation code on success

---
+++ tests/infra/db/mongodb/commands/add-confirmation-code-mongo-repository.spec.ts
@@ -0,0 +1,43 @@
+import { ObjectId, type Collection } from 'mongodb'
+import MockDate from 'mockdate'
+
+import { connectToDatabase, disconnectFromDatabase, clearCollection, getCollection } from '@/tests/infra/db/mongodb'
+import { AddConfirmationCodeMongoRepository } from '@/infra/db/mongodb/commands'
+import { faker } from '@faker-js/faker'
+
+let codeCollection: Collection
+
+const makeSut = (): AddConfirmationCodeMongoRepository => {
+  return new AddConfirmationCodeMongoRepository()
+}
+
+describe('AddConfirmationCodeMongoRepository', () => {
+  beforeAll(async() => {
+    MockDate.set(new Date())
+    await connectToDatabase()
+  })
+
+  afterAll(async() => {
+    MockDate.reset()
+    await disconnectFromDatabase()
+  })
+
+  beforeEach(async() => {
+    codeCollection = await getCollection('codes')
+    await clearCollection(codeCollection)
+  })
+
+  test('Should add a confirmation code on success', async() => {
+    const sut = makeSut()
+    const confirmationCode = faker.string.alphanumeric(6)
+    const id = await sut.add(confirmationCode)
+    const count = await codeCollection.countDocuments()
+    const code = await codeCollection.findOne({ _id: new ObjectId(id) })
+    const expirationDate = new Date()
+    expirationDate.setMinutes(expirationDate.getMinutes() + 10)
+
+    expect(count).toBe(1)
+    expect(code?.confirmationCode).toBe(confirmationCode)
+    expect(code?.expirationDate).toEqual(expirationDate)
+  })
+})
