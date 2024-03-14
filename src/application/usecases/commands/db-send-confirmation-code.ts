aebb696b935039e7c4fe9e33d5a759cfd562fe43
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-14 01:10:09 -0300

    feat: ensure DbSendConfirmationCode throws AccountNotFoundError if CheckAccountByEmailRepository returns false

--- src/application/usecases/commands/db-send-confirmation-code.ts
+++ src/application/usecases/commands/db-send-confirmation-code.ts
@@ -1,10 +1,14 @@
 import { type CheckAccountByEmailRepository } from '@/application/protocols/queries'
 import { type SendConfirmationCode } from '@/domain/usecases/commands'
+import { AccountNotFoundError } from '@/domain/errors'
 
 export class DbSendConfirmationCode implements SendConfirmationCode {
   constructor(private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository) {}
 
   public async send(email: string): Promise<void> {
-    await this.checkAccountByEmailRepository.check(email)
+    const account = await this.checkAccountByEmailRepository.check(email)
+    if (!account) {
+      throw new AccountNotFoundError()
+    }
   }
 }
