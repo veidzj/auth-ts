dcbfbf29ce39eae2abac003f8c9562b40bc3d278
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-14 20:24:12 -0300

    refactor: use crypto to generate code numbers

--- src/application/helpers/generate-confirmation-code.ts
+++ src/application/helpers/generate-confirmation-code.ts
@@ -1,8 +1,10 @@
+import crypto from 'crypto'
+
 export class GenerateConfirmationCode {
   public static generate(length: number = 6): string {
     let code = ''
     for (let i = 0; i < length; i++) {
-      code += Math.floor(Math.random() * 10).toString()
+      code += crypto.randomInt(10).toString()
     }
     return code
   }
