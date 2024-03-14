fc52c30e23421c0333101db0c89ca6e519246870
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-14 01:07:54 -0300

    refactor: move sut creation to a factory

--- tests/application/usecases/commands/db-send-confirmation-code.spec.ts
+++ tests/application/usecases/commands/db-send-confirmation-code.spec.ts
@@ -3,13 +3,26 @@ import { faker } from '@faker-js/faker'
 import { DbSendConfirmationCode } from '@/application/usecases/commands'
 import { CheckAccountByEmailRepositorySpy } from '@/tests/application/mocks/queries'
 
+interface Sut {
+  sut: DbSendConfirmationCode
+  checkAccountByEmailRepositorySpy: CheckAccountByEmailRepositorySpy
+}
+
+const makeSut = (): Sut => {
+  const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
+  const sut = new DbSendConfirmationCode(checkAccountByEmailRepositorySpy)
+  return {
+    sut,
+    checkAccountByEmailRepositorySpy
+  }
+}
+
 const email: string = faker.internet.email()
 
 describe('DbSendConfirmationCode', () => {
   describe('CheckAccountByEmailRepository', () => {
     test('Should call CheckAccountByEmailRepository with correct email', async() => {
-      const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
-      const sut = new DbSendConfirmationCode(checkAccountByEmailRepositorySpy)
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
       await sut.send(email)
       expect(checkAccountByEmailRepositorySpy.email).toBe(email)
     })
