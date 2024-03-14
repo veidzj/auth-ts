13009b4c5df272932d9031a8921d25779d5ac42c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-14 15:59:26 -0300

    refactor: move sut to direct code

--- tests/application/helpers/generate-confirmation-code.spec.ts
+++ tests/application/helpers/generate-confirmation-code.spec.ts
@@ -2,29 +2,25 @@ import { GenerateConfirmationCode } from '@/application/helpers'
 
 describe('GenerateConfirmationCode', () => {
   test('Should generate a code of default length 6', () => {
-    const sut = GenerateConfirmationCode
-    const code = sut.generate()
+    const code = GenerateConfirmationCode.generate()
     expect(code).toHaveLength(6)
   })
 
   test('Should generate a numeric code', () => {
-    const sut = GenerateConfirmationCode
-    const code = sut.generate()
+    const code = GenerateConfirmationCode.generate()
     expect(code).toMatch(/^\d+$/)
   })
 
   test.each([1, 5, 10, 20])('Should generate a code of length %i', (length) => {
-    const sut = GenerateConfirmationCode
-    const code = sut.generate(length)
+    const code = GenerateConfirmationCode.generate(length)
     expect(code).toHaveLength(length)
     expect(code).toMatch(/^\d+$/)
   })
 
   test('Should generate unique codes on subsequent calls', () => {
-    const sut = GenerateConfirmationCode
     const codes = new Set()
     for (let i = 0; i < 100; i++) {
-      codes.add(sut.generate())
+      codes.add(GenerateConfirmationCode.generate())
     }
     expect(codes.size).toBe(100)
   })
