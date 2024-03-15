c4a36d5daa8c28f7c5924db27c73b70d59a0f207
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-15 17:17:01 -0300

    test: ensure ActivateAccountController returns serverError if Validation throws

--- tests/presentation/controllers/commands/activate-account-controller.spec.ts
+++ tests/presentation/controllers/commands/activate-account-controller.spec.ts
@@ -48,6 +48,15 @@ describe('ActivateAccountController', () => {
 
       expect(httpResponse).toEqual(HttpHelper.badRequest(new ValidationError(errorMessage)))
     })
+
+    test('Should return serverError if Validation throws', async() => {
+      const { sut, validationSpy } = makeSut()
+      jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new Error() })
+
+      const httpResponse = await sut.handle(mockRequest())
+
+      expect(httpResponse).toEqual(HttpHelper.serverError())
+    })
   })
 
   describe('ActivateAccount', () => {
