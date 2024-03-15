567e4b4a136463898ec4f11ac84a209cfc742f08
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-15 17:15:43 -0300

    feat: ensure ActivateAccountController returns badRequest if Validation throws ValidationError

--- src/presentation/controllers/commands/activate-account-controller.ts
+++ src/presentation/controllers/commands/activate-account-controller.ts
@@ -1,5 +1,6 @@
 import { type Controller, type HttpResponse, type Validation } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
+import { ValidationError } from '@/validation/errors'
 import { type ActivateAccount } from '@/domain/usecases/commands'
 import { AccountNotFoundError, AccountAlreadyActivatedError } from '@/domain/errors'
 
@@ -15,6 +16,9 @@ export class ActivateAccountController implements Controller {
       await this.activateAccount.activate(request.accountId)
       return HttpHelper.noContent()
     } catch (error) {
+      if (error instanceof ValidationError) {
+        return HttpHelper.badRequest(error)
+      }
       if (error instanceof AccountNotFoundError) {
         return HttpHelper.notFound(error)
       }
