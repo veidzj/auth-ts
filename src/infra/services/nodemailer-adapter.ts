cbb3ff86d575aa3a7ab79fd918b2987ed2aa0008
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-14 23:54:21 -0300

    feat: ensure NodemailerAdapter calls sendMail with correct values

---
+++ src/infra/services/nodemailer-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer, { type Transporter, type SendMailOptions } from 'nodemailer'
+import type SMTPTransport from 'nodemailer/lib/smtp-transport'
+
+import { type SendConfirmationCodeToEmail } from '@/application/protocols/services'
+
+export class NodemailerAdapter implements SendConfirmationCodeToEmail {
+  private readonly transporter: Transporter
+
+  constructor(transporterConfig: SMTPTransport.Options) {
+    this.transporter = nodemailer.createTransport(transporterConfig)
+  }
+
+  public async send(confirmationCode: string, email: string): Promise<void> {
+    const mailOptions: SendMailOptions = {
+      from: 'Auth TS',
+      to: email,
+      subject: 'Your confirmation code',
+      text: `Your confirmation code is: ${confirmationCode}`,
+      html: `<b>Your confirmation code is:</b> ${confirmationCode}`
+    }
+    await this.transporter.sendMail(mailOptions)
+  }
+}
