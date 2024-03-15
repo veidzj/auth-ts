64e44fd4214a15d49cc13b896038274a83f9225d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-15 00:01:07 -0300

    refactor: move sut creation to a factory

--- tests/infra/services/nodemailer-adapter.spec.ts
+++ tests/infra/services/nodemailer-adapter.spec.ts
@@ -9,18 +9,31 @@ jest.mock('nodemailer', () => ({
   })
 }))
 
+interface Sut {
+  sut: NodemailerAdapter
+  transporterConfig: SMTPTransport.Options
+}
+
+const makeSut = (): Sut => {
+  const transporterConfig: SMTPTransport.Options = {
+    host: 'smtp.test.com',
+    port: 587,
+    auth: {
+      user: 'test@test.com',
+      pass: 'test'
+    }
+  }
+  const sut = new NodemailerAdapter(transporterConfig)
+  return {
+    sut,
+    transporterConfig
+  }
+}
+
 describe('NodemailerAdapter', () => {
   test('Should call sendMail with correct values', async() => {
-    const transporterConfig: SMTPTransport.Options = {
-      host: 'smtp.test.com',
-      port: 587,
-      auth: {
-        user: 'test@test.com',
-        pass: 'test'
-      }
-    }
+    const { sut, transporterConfig } = makeSut()
     const sendMailSpy = jest.spyOn(nodemailer.createTransport(transporterConfig), 'sendMail')
-    const sut = new NodemailerAdapter(transporterConfig)
     const confirmationCode = '123456'
     const email = 'test@example.com'
     await sut.send(confirmationCode, email)
