1beee3c766f4d2c8e1876ad856ba656c98c9c9aa
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-16 13:55:21 -0300

    feat: ensure ChangePasswordController returns badRequest if Validation throws ValidationError

--- src/presentation/controllers/commands/change-password-controller.ts
+++ src/presentation/controllers/commands/change-password-controller.ts
@@ -1,5 +1,6 @@
 import { type Controller, type HttpResponse, type Validation } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
+import { ValidationError } from '@/validation/errors'
 
 export class ChangePasswordController implements Controller {
   constructor(
@@ -7,8 +8,15 @@ export class ChangePasswordController implements Controller {
   ) {}
 
   public async handle(request: ChangePasswordController.Request): Promise<HttpResponse> {
-    this.validation.validate(request)
-    return HttpHelper.noContent()
+    try {
+      this.validation.validate(request)
+      return HttpHelper.noContent()
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return HttpHelper.badRequest(error)
+      }
+      return HttpHelper.serverError(error as Error)
+    }
   }
 }
 
