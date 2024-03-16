64decde4d57f58899c18f1a66d00c0fc074391cd
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-16 16:34:41 -0300

    refactor: move sut creation to a factory

--- tests/presentation/controllers/commands/change-password-controller.spec.ts
+++ tests/presentation/controllers/commands/change-password-controller.spec.ts
@@ -5,6 +5,20 @@ import { ChangePasswordController } from '@/presentation/controllers/commands'
 import { HttpHelper } from '@/presentation/helpers'
 import { ValidationError } from '@/validation/errors'
 
+interface Sut {
+  sut: ChangePasswordController
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): Sut => {
+  const validationSpy = new ValidationSpy()
+  const sut = new ChangePasswordController(validationSpy)
+  return {
+    sut,
+    validationSpy
+  }
+}
+
 const mockRequest = (): ChangePasswordController.Request => ({
   email: faker.internet.email(),
   newPassword: faker.internet.password()
@@ -13,16 +27,14 @@ const mockRequest = (): ChangePasswordController.Request => ({
 describe('ChangePasswordController', () => {
   describe('Validation', () => {
     test('Should call Validation with correct values', async() => {
-      const validationSpy = new ValidationSpy()
-      const sut = new ChangePasswordController(validationSpy)
+      const { sut, validationSpy } = makeSut()
       const request = mockRequest()
       await sut.handle(request)
       expect(validationSpy.input).toEqual(request)
     })
 
     test('Should return badRequest if Validation throws ValidationError', async() => {
-      const validationSpy = new ValidationSpy()
-      const sut = new ChangePasswordController(validationSpy)
+      const { sut, validationSpy } = makeSut()
       const errorMessage = faker.word.words()
       jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new ValidationError(errorMessage) })
       const httpResponse = await sut.handle(mockRequest())
@@ -30,8 +42,7 @@ describe('ChangePasswordController', () => {
     })
 
     test('Should return serverError if Validation throws', async() => {
-      const validationSpy = new ValidationSpy()
-      const sut = new ChangePasswordController(validationSpy)
+      const { sut, validationSpy } = makeSut()
       jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new Error() })
       const httpResponse = await sut.handle(mockRequest())
       expect(httpResponse).toEqual(HttpHelper.serverError(new Error()))
