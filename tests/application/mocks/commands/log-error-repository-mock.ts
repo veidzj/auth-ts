977c15a3e3ff510bf25b56e9a4d715be40371758
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-16 10:56:23 -0300

    refactor: change LogErrorRepository to return error id

--- tests/application/mocks/commands/log-error-repository-mock.ts
+++ tests/application/mocks/commands/log-error-repository-mock.ts
@@ -2,8 +2,10 @@ import { type LogErrorRepository } from '@/application/protocols/commands'
 
 export class LogErrorRepositorySpy implements LogErrorRepository {
   public stack: string
+  public errorId: string
 
-  public async log(stack: string): Promise<void> {
+  public async log(stack: string): Promise<string> {
     this.stack = stack
+    return this.errorId
   }
 }
