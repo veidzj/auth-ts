a43aee1f0caa857b45293deb42b243b15654c4c8
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-16 18:14:07 -0300

    feat: ensure DbChangePassword calls ChangePasswordRepository with correct values

--- src/application/usecases/commands/db-change-password.ts
+++ src/application/usecases/commands/db-change-password.ts
@@ -1,16 +1,19 @@
 import { type CheckAccountByEmailRepository } from '@/application/protocols/queries'
+import { type ChangePasswordRepository } from '@/application/protocols/commands'
 import { type ChangePassword } from '@/domain/usecases/commands'
 import { AccountNotFoundError } from '@/domain/errors'
 
 export class DbChangePassword implements ChangePassword {
   constructor(
-    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository
+    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository,
+    private readonly changePasswordRepository: ChangePasswordRepository
   ) {}
 
-  public async change(email: string, password: string): Promise<void> {
+  public async change(email: string, newPassword: string): Promise<void> {
     const accountExists = await this.checkAccountByEmailRepository.check(email)
     if (!accountExists) {
       throw new AccountNotFoundError()
     }
+    await this.changePasswordRepository.change(email, newPassword)
   }
 }
