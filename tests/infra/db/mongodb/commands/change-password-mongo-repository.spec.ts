e9d173d641bf2beb00336691a705aebebeaf6dbf
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-16 18:28:42 -0300

    test: ensure ChangePasswordMongoRepository throws if mongo throws

--- tests/infra/db/mongodb/commands/change-password-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/commands/change-password-mongo-repository.spec.ts
@@ -1,4 +1,4 @@
-import { type Collection } from 'mongodb'
+import { Collection } from 'mongodb'
 import MockDate from 'mockdate'
 import { faker } from '@faker-js/faker'
 
@@ -56,5 +56,14 @@ describe('ChangePasswordMongoRepository', () => {
       expect(account?.password).toBe(fakeAccount.password)
       expect(account?.updatedAt).toBeFalsy()
     }
+  })
+
+  test('Should throw if mongo throws', async() => {
+    const sut = makeSut()
+    jest.spyOn(Collection.prototype, 'updateOne').mockImplementationOnce(() => { throw new Error() })
+
+    const promise = sut.change(faker.internet.email(), faker.internet.email())
+
+    await expect(promise).rejects.toThrow()
   })
 })
