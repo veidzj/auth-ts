848b80ceddb26acd2224c07d203bac6193b3a30c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-16 18:11:14 -0300

    refactor: move sut creation to a factory

--- tests/application/usecases/commands/db-change-password.spec.ts
+++ tests/application/usecases/commands/db-change-password.spec.ts
@@ -4,32 +4,49 @@ import { CheckAccountByEmailRepositorySpy } from '@/tests/application/mocks/quer
 import { DbChangePassword } from '@/application/usecases/commands'
 import { AccountNotFoundError } from '@/domain/errors'
 
+interface Sut {
+  sut: DbChangePassword
+  checkAccountByEmailRepositorySpy: CheckAccountByEmailRepositorySpy
+}
+
+const makeSut = (): Sut => {
+  const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
+  checkAccountByEmailRepositorySpy.output = true
+  const sut = new DbChangePassword(checkAccountByEmailRepositorySpy)
+  return {
+    sut,
+    checkAccountByEmailRepositorySpy
+  }
+}
+
 const email: string = faker.internet.email()
 const newPassword: string = faker.internet.password()
 
 describe('DbChangePassword', () => {
   describe('CheckAccountByEmailRepository', () => {
     test('Should call CheckAccountByEmailRepository with correct email', async() => {
-      const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
-      checkAccountByEmailRepositorySpy.output = true
-      const sut = new DbChangePassword(checkAccountByEmailRepositorySpy)
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
+
       await sut.change(email, newPassword)
+
       expect(checkAccountByEmailRepositorySpy.email).toBe(email)
     })
 
     test('Should throw AccountNotFoundError if CheckAccountByEmailRepository returns false', async() => {
-      const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
-      const sut = new DbChangePassword(checkAccountByEmailRepositorySpy)
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
       checkAccountByEmailRepositorySpy.output = false
+
       const promise = sut.change(email, newPassword)
+
       await expect(promise).rejects.toThrow(new AccountNotFoundError())
     })
 
     test('Should throw if CheckAccountByEmailRepository throws', async() => {
-      const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
-      const sut = new DbChangePassword(checkAccountByEmailRepositorySpy)
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
       jest.spyOn(checkAccountByEmailRepositorySpy, 'check').mockRejectedValue(new Error())
+
       const promise = sut.change(email, newPassword)
+      
       await expect(promise).rejects.toThrow()
     })
   })
