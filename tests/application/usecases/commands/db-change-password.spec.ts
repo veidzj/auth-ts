15e3f53c5eb797ae129400344df61d5ce43592c5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-16 17:21:39 -0300

    test: ensure DbChangePassword throws AccountNotFoundError if CheckAccountByEmailRepository returns false

--- tests/application/usecases/commands/db-change-password.spec.ts
+++ tests/application/usecases/commands/db-change-password.spec.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker'
 
 import { CheckAccountByEmailRepositorySpy } from '@/tests/application/mocks/queries'
 import { DbChangePassword } from '@/application/usecases/commands'
+import { AccountNotFoundError } from '@/domain/errors'
 
 const email: string = faker.internet.email()
 const newPassword: string = faker.internet.password()
@@ -10,9 +11,18 @@ describe('DbChangePassword', () => {
   describe('CheckAccountByEmailRepository', () => {
     test('Should call CheckAccountByEmailRepository with correct email', async() => {
       const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
+      checkAccountByEmailRepositorySpy.output = true
       const sut = new DbChangePassword(checkAccountByEmailRepositorySpy)
       await sut.change(email, newPassword)
       expect(checkAccountByEmailRepositorySpy.email).toBe(email)
+    })
+
+    test('Should throw AccountNotFoundError if CheckAccountByEmailRepository returns false', async() => {
+      const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
+      const sut = new DbChangePassword(checkAccountByEmailRepositorySpy)
+      checkAccountByEmailRepositorySpy.output = false
+      const promise = sut.change(email, newPassword)
+      await expect(promise).rejects.toThrow(new AccountNotFoundError())
     })
   })
 })
