8a3a261800470a91fc04f46533cba95260544c5c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-16 08:32:23 -0300

    refactor: move sut creation to a factory

--- tests/main/decorators/log-controller-decorator.spec.ts
+++ tests/main/decorators/log-controller-decorator.spec.ts
@@ -1,8 +1,21 @@
 import { faker } from '@faker-js/faker'
 
+import { ControllerSpy } from '@/tests/presentation/mocks'
 import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
-import { type Controller, type HttpResponse } from '@/presentation/protocols'
-import { HttpHelper } from '@/presentation/helpers'
+
+interface Sut {
+  sut: LogControllerDecorator
+  controllerSpy: ControllerSpy
+}
+
+const makeSut = (): Sut => {
+  const controllerSpy = new ControllerSpy()
+  const sut = new LogControllerDecorator(controllerSpy)
+  return {
+    sut,
+    controllerSpy
+  }
+}
 
 const mockRequest = (): object => ({
   field: faker.word.words()
@@ -11,17 +24,7 @@ const mockRequest = (): object => ({
 describe('LogControllerDecorator', () => {
   describe('Controller', () => {
     test('Should call Controller with correct values', async() => {
-      class ControllerSpy implements Controller {
-        public request: object
-        public httpResponse = HttpHelper.ok({ message: faker.word.words() })
-
-        async handle(request: object): Promise<HttpResponse> {
-          this.request = request
-          return this.httpResponse
-        }
-      }
-      const controllerSpy = new ControllerSpy()
-      const sut = new LogControllerDecorator(controllerSpy)
+      const { sut, controllerSpy } = makeSut()
       const request = mockRequest()
       await sut.handle(request)
       expect(controllerSpy.request).toEqual(request)
