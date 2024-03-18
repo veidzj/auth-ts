0e2edf0683abf1fd5e0e089ee94a467c7709bc9d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-18 10:05:40 -0300

    refactor: change FullNameValidation to receive a field name

--- src/main/factories/validators/sign-up-validation-factory.ts
+++ src/main/factories/validators/sign-up-validation-factory.ts
@@ -14,7 +14,7 @@ export class SignUpValidationFactory {
   public static makeSignUpValidation(): ValidationComposite {
     const validations: Validation[] = []
     validations.push(new UsernameValidation())
-    validations.push(new FullNameValidation())
+    validations.push(new FullNameValidation('fullName'))
     validations.push(new EmailValidation(new EmailValidatorAdapter(), 'email'))
     validations.push(new PasswordValidation('password'))
     validations.push(new ProfileImageValidation(new URLValidatorAdapter()))
--- src/validation/validators/full-name-validation.ts
+++ src/validation/validators/full-name-validation.ts
@@ -2,24 +2,21 @@ import { ValidationError } from '@/validation/errors'
 import { type Validation } from '@/presentation/protocols'
 
 export class FullNameValidation implements Validation {
-  public validate(input: FullNameValidation.Input): void {
-    if (input.fullName.length < 3 || input.fullName.length > 50) {
-      throw new ValidationError('Full name must be between 3 and 50 characters long')
-    }
-    if (!/^[A-Z]/.test(input.fullName)) {
-      throw new ValidationError('Full name must start with an uppercase letter')
-    }
-    if (/\s{2,}|^\s|\s$/.test(input.fullName)) {
-      throw new ValidationError('Full name must be separated by a single space')
-    }
-    if (!/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/.test(input.fullName)) {
-      throw new ValidationError('Full name can only contain letters and letters with accents')
-    }
-  }
-}
+  constructor(private readonly fieldName: string) {}
 
-export namespace FullNameValidation {
-  export interface Input {
-    fullName: string
+  public validate(input: object): void {
+    const fullName: string = input[this.fieldName]
+    if (fullName.length < 3 || fullName.length > 50) {
+      throw new ValidationError(`${this.fieldName} must be between 3 and 50 characters long`)
+    }
+    if (!/^[A-Z]/.test(fullName)) {
+      throw new ValidationError(`${this.fieldName} must start with an uppercase letter`)
+    }
+    if (/\s{2,}|^\s|\s$/.test(fullName)) {
+      throw new ValidationError(`${this.fieldName} must be separated by a single space`)
+    }
+    if (!/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/.test(fullName)) {
+      throw new ValidationError(`${this.fieldName} can only contain letters and letters with accents`)
+    }
   }
 }
