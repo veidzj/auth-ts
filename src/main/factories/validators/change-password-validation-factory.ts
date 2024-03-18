249345738fb29811e73474f0893308638b665a70
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-18 09:44:24 -0300

    refactor: remove EmailValidation from ChangePasswordValidationFactory

--- src/main/factories/validators/change-password-validation-factory.ts
+++ src/main/factories/validators/change-password-validation-factory.ts
@@ -1,15 +1,12 @@
 import { type Validation } from '@/presentation/protocols'
 import {
   ValidationComposite,
-  EmailValidation,
   PasswordValidation
 } from '@/validation/validators'
-import { EmailValidatorAdapter } from '@/infra/validators'
 
 export class ChangePasswordValidationFactory {
   public static makeChangePasswordValidation(): ValidationComposite {
     const validations: Validation[] = []
-    validations.push(new EmailValidation(new EmailValidatorAdapter(), 'email'))
     validations.push(new PasswordValidation('newPassword'))
     return new ValidationComposite(validations)
   }
