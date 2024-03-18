050e4cf6b4af4c204e74afbe7b95e720a327f73d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-18 11:01:20 -0300

    refactor: add RequiredFieldValidation to all validations factory

--- src/main/factories/validators/change-password-validation-factory.ts
+++ src/main/factories/validators/change-password-validation-factory.ts
@@ -1,12 +1,14 @@
 import { type Validation } from '@/presentation/protocols'
 import {
   ValidationComposite,
+  RequiredFieldValidation,
   PasswordValidation
 } from '@/validation/validators'
 
 export class ChangePasswordValidationFactory {
   public static makeChangePasswordValidation(): ValidationComposite {
     const validations: Validation[] = []
+    validations.push(new RequiredFieldValidation('newPassword'))
     validations.push(new PasswordValidation('newPassword'))
     return new ValidationComposite(validations)
   }
