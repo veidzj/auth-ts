7117dd3a87616c2ee0801743c291370134147232
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-18 22:28:16 -0300

    refactor: add verification to current email in DbChangeEmail

--- tests/application/usecases/commands/db-change-email.spec.ts
+++ tests/application/usecases/commands/db-change-email.spec.ts
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker'
 import { DbChangeEmail } from '@/application/usecases/commands'
 import { CheckAccountByEmailRepositorySpy } from '@/tests/application/mocks/queries'
 import { ChangeEmailRepositorySpy } from '@/tests/application/mocks/commands'
-import { AccountAlreadyExistsError } from '@/domain/errors'
+import { AccountNotFoundError, AccountAlreadyExistsError } from '@/domain/errors'
 
 interface Sut {
   sut: DbChangeEmail
@@ -30,15 +30,31 @@ describe('DbChangeEmail', () => {
     test('Should call CheckAccountByEmailRepository with correct email', async() => {
       const { sut, checkAccountByEmailRepositorySpy } = makeSut()
       jest.spyOn(checkAccountByEmailRepositorySpy, 'check')
+        .mockResolvedValueOnce(true)
+        .mockResolvedValueOnce(false)
 
       await sut.change(currentEmail, newEmail)
 
-      expect(checkAccountByEmailRepositorySpy.check).toHaveBeenCalledWith(newEmail)
+      expect(checkAccountByEmailRepositorySpy.check).toHaveBeenNthCalledWith(1, currentEmail)
+      expect(checkAccountByEmailRepositorySpy.check).toHaveBeenNthCalledWith(2, newEmail)
     })
 
-    test('Should throw AccountAlreadyExistsError if CheckAccountByEmailRepository returns true', async() => {
+    test('Should throw AccountNotFoundError if first CheckAccountByEmailRepository call returns false', async() => {
       const { sut, checkAccountByEmailRepositorySpy } = makeSut()
-      jest.spyOn(checkAccountByEmailRepositorySpy, 'check').mockReturnValueOnce(Promise.resolve(true))
+      jest.spyOn(checkAccountByEmailRepositorySpy, 'check')
+        .mockResolvedValueOnce(false)
+        .mockResolvedValueOnce(false)
+
+      const promise = sut.change(currentEmail, newEmail)
+
+      await expect(promise).rejects.toThrow(new AccountNotFoundError())
+    })
+
+    test('Should throw AccountAlreadyExistsError if second CheckAccountByEmailRepository call returns true', async() => {
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
+      jest.spyOn(checkAccountByEmailRepositorySpy, 'check')
+        .mockResolvedValueOnce(true)
+        .mockResolvedValueOnce(true)
 
       const promise = sut.change(currentEmail, newEmail)
 
@@ -57,7 +73,10 @@ describe('DbChangeEmail', () => {
 
   describe('ChangeEmailRepository', () => {
     test('Should call ChangeEmailRepository with correct emails', async() => {
-      const { sut, changeEmailRepositorySpy } = makeSut()
+      const { sut, checkAccountByEmailRepositorySpy, changeEmailRepositorySpy } = makeSut()
+      jest.spyOn(checkAccountByEmailRepositorySpy, 'check')
+        .mockResolvedValueOnce(true)
+        .mockResolvedValueOnce(false)
       jest.spyOn(changeEmailRepositorySpy, 'change')
 
       await sut.change(currentEmail, newEmail)
@@ -66,7 +85,10 @@ describe('DbChangeEmail', () => {
     })
 
     test('Should throw if ChangeEmailRepository throws', async() => {
-      const { sut, changeEmailRepositorySpy } = makeSut()
+      const { sut, checkAccountByEmailRepositorySpy, changeEmailRepositorySpy } = makeSut()
+      jest.spyOn(checkAccountByEmailRepositorySpy, 'check')
+        .mockResolvedValueOnce(true)
+        .mockResolvedValueOnce(false)
       jest.spyOn(changeEmailRepositorySpy, 'change').mockRejectedValueOnce(new Error())
 
       const promise = sut.change(currentEmail, newEmail)
@@ -75,7 +97,10 @@ describe('DbChangeEmail', () => {
     })
 
     test('Should not throw on success', async() => {
-      const { sut } = makeSut()
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
+      jest.spyOn(checkAccountByEmailRepositorySpy, 'check')
+        .mockResolvedValueOnce(true)
+        .mockResolvedValueOnce(false)
 
       const promise = sut.change(currentEmail, newEmail)
 
