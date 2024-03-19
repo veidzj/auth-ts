52e4fbb17e0aa889f8555ea7d432f383507b1330
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-19 15:42:41 -0300

    test: ensure DbVerificationCode throws InvalidOrExpiredConfirmationCodeError if VerifyConfirmationCodeRepository returns false

--- tests/application/usecases/queries/db-verify-confirmation-code.spec.ts
+++ tests/application/usecases/queries/db-verify-confirmation-code.spec.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker'
 
 import { VerifyConfirmationCodeRepositorySpy } from '@/tests/application/mocks/queries'
 import { DbVerifyConfirmationCode } from '@/application/usecases/queries'
+import { InvalidOrExpiredConfirmationCodeError } from '@/domain/errors'
 
 const accountId: string = faker.string.uuid()
 const confirmationCode: string = faker.string.alphanumeric(6)
@@ -13,5 +14,13 @@ describe('DbVerifyConfirmationCode', () => {
     await sut.verify(accountId, confirmationCode)
     expect(verifyConfirmationCodeRepositorySpy.accountId).toBe(accountId)
     expect(verifyConfirmationCodeRepositorySpy.confirmationCode).toBe(confirmationCode)
+  })
+
+  test('Should throw InvalidOrExpiredConfirmationCodeError if VerifyConfirmationCodeRepository returns false', async() => {
+    const verifyConfirmationCodeRepositorySpy = new VerifyConfirmationCodeRepositorySpy()
+    verifyConfirmationCodeRepositorySpy.output = false
+    const sut = new DbVerifyConfirmationCode(verifyConfirmationCodeRepositorySpy)
+    const promise = sut.verify(accountId, confirmationCode)
+    await expect(promise).rejects.toThrow(new InvalidOrExpiredConfirmationCodeError())
   })
 })
