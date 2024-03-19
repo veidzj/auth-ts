e19ff5adcf2fd4e2e383561a3e00e88fa9ce1275
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-19 18:51:55 -0300

    test: ensure VerifyConfirmationCodeMongoRepository returns true if confirmation code is valid, account id is correct and it is not expired

---
+++ tests/infra/db/mongodb/queries/verify-confirmation-code-mongo-repository.spec.ts
@@ -0,0 +1,43 @@
+import { type Collection } from 'mongodb'
+import { faker } from '@faker-js/faker'
+
+import { connectToDatabase, disconnectFromDatabase, clearCollection, getCollection } from '@/tests/infra/db/mongodb'
+import { VerifyConfirmationCodeMongoRepository } from '@/infra/db/mongodb/queries'
+
+let codeCollection: Collection
+
+const makeSut = (): VerifyConfirmationCodeMongoRepository => {
+  return new VerifyConfirmationCodeMongoRepository()
+}
+
+describe('VerifyConfirmationCodeMongoRepository', () => {
+  beforeAll(async() => {
+    await connectToDatabase()
+  })
+
+  afterAll(async() => {
+    await disconnectFromDatabase()
+  })
+
+  beforeEach(async() => {
+    codeCollection = await getCollection('codes')
+    await clearCollection(codeCollection)
+  })
+
+  test('Should return true if confirmation code is valid, account id is correct and it is not expired', async() => {
+    const sut = makeSut()
+    const accountId = faker.database.mongodbObjectId()
+    const confirmationCode = faker.string.alphanumeric(6)
+    const expirationDate = new Date()
+    expirationDate.setMinutes(expirationDate.getMinutes() + 10)
+    await codeCollection.insertOne({
+      accountId,
+      confirmationCode,
+      expirationDate
+    })
+
+    const confirmationCodeIsValid = await sut.verify(accountId, confirmationCode)
+
+    expect(confirmationCodeIsValid).toBe(true)
+  })
+})
