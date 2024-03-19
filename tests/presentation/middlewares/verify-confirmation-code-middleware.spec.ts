c83715dd4430ddebdfd6d33532be49ade4dc3b24
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-19 13:08:37 -0300

    test: ensure VerifyConfirmationCodeMiddleware returns badRequest if VerifyConfirmationCode throws InvalidOrExpiredConfirmationCodeError

--- tests/presentation/middlewares/verify-confirmation-code-middleware.spec.ts
+++ tests/presentation/middlewares/verify-confirmation-code-middleware.spec.ts
@@ -3,6 +3,7 @@ import { faker } from '@faker-js/faker'
 import { VerifyConfirmationCodeSpy } from '@/tests/domain/mocks/queries'
 import { VerifyConfirmationCodeMiddleware } from '@/presentation/middlewares'
 import { HttpHelper } from '@/presentation/helpers'
+import { InvalidOrExpiredConfirmationCodeError } from '@/domain/errors'
 
 interface Sut {
   sut: VerifyConfirmationCodeMiddleware
@@ -27,21 +28,36 @@ describe('VerifyConfirmationCodeMiddleware', () => {
   test('Should call VerifyConfirmationCode with correct values', async() => {
     const { sut, verifyConfirmationCodeSpy } = makeSut()
     const request = mockRequest()
+
     await sut.handle(request)
+
     expect(verifyConfirmationCodeSpy.accountId).toBe(request.accountId)
     expect(verifyConfirmationCodeSpy.confirmationCode).toBe(request.confirmationCode)
+  })
+
+  test('Should return badRequest if VerifyConfirmationCode throws InvalidOrExpiredConfirmationCodeError', async() => {
+    const { sut, verifyConfirmationCodeSpy } = makeSut()
+    jest.spyOn(verifyConfirmationCodeSpy, 'verify').mockRejectedValueOnce(new InvalidOrExpiredConfirmationCodeError())
+
+    const httpResponse = await sut.handle(mockRequest())
+
+    expect(httpResponse).toEqual(HttpHelper.badRequest(new InvalidOrExpiredConfirmationCodeError()))
   })
 
   test('Should return serverError if VerifyConfirmationCode throws', async() => {
     const { sut, verifyConfirmationCodeSpy } = makeSut()
     jest.spyOn(verifyConfirmationCodeSpy, 'verify').mockRejectedValueOnce(new Error())
+
     const httpResponse = await sut.handle(mockRequest())
+
     expect(httpResponse).toEqual(HttpHelper.serverError(new Error()))
   })
 
   test('Should return noContent on success', async() => {
     const { sut } = makeSut()
+
     const httpResponse = await sut.handle(mockRequest())
+
     expect(httpResponse).toEqual(HttpHelper.noContent())
   })
 })
