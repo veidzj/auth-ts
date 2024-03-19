b8660391dccb5cdfec64891fc11b4fbf549883bb
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-19 16:55:58 -0300

    refactor: move sut creation to a factory

--- tests/application/usecases/queries/db-verify-confirmation-code.spec.ts
+++ tests/application/usecases/queries/db-verify-confirmation-code.spec.ts
@@ -4,30 +4,41 @@ import { VerifyConfirmationCodeRepositorySpy } from '@/tests/application/mocks/q
 import { DbVerifyConfirmationCode } from '@/application/usecases/queries'
 import { InvalidOrExpiredConfirmationCodeError } from '@/domain/errors'
 
+interface Sut {
+  sut: DbVerifyConfirmationCode
+  verifyConfirmationCodeRepositorySpy: VerifyConfirmationCodeRepositorySpy
+}
+
+const makeSut = (): Sut => {
+  const verifyConfirmationCodeRepositorySpy = new VerifyConfirmationCodeRepositorySpy()
+  const sut = new DbVerifyConfirmationCode(verifyConfirmationCodeRepositorySpy)
+  return {
+    sut,
+    verifyConfirmationCodeRepositorySpy
+  }
+}
+
 const accountId: string = faker.string.uuid()
 const confirmationCode: string = faker.string.alphanumeric(6)
 
 describe('DbVerifyConfirmationCode', () => {
   test('Should call VerifyConfirmationCodeRepository with correct values', async() => {
-    const verifyConfirmationCodeRepositorySpy = new VerifyConfirmationCodeRepositorySpy()
-    const sut = new DbVerifyConfirmationCode(verifyConfirmationCodeRepositorySpy)
+    const { sut, verifyConfirmationCodeRepositorySpy } = makeSut()
     await sut.verify(accountId, confirmationCode)
     expect(verifyConfirmationCodeRepositorySpy.accountId).toBe(accountId)
     expect(verifyConfirmationCodeRepositorySpy.confirmationCode).toBe(confirmationCode)
   })
 
   test('Should throw InvalidOrExpiredConfirmationCodeError if VerifyConfirmationCodeRepository returns false', async() => {
-    const verifyConfirmationCodeRepositorySpy = new VerifyConfirmationCodeRepositorySpy()
+    const { sut, verifyConfirmationCodeRepositorySpy } = makeSut()
     verifyConfirmationCodeRepositorySpy.output = false
-    const sut = new DbVerifyConfirmationCode(verifyConfirmationCodeRepositorySpy)
     const promise = sut.verify(accountId, confirmationCode)
     await expect(promise).rejects.toThrow(new InvalidOrExpiredConfirmationCodeError())
   })
 
   test('Should throw if VerifyConfirmationCodeRepository throws', async() => {
-    const verifyConfirmationCodeRepositorySpy = new VerifyConfirmationCodeRepositorySpy()
+    const { sut, verifyConfirmationCodeRepositorySpy } = makeSut()
     jest.spyOn(verifyConfirmationCodeRepositorySpy, 'verify').mockRejectedValueOnce(new Error())
-    const sut = new DbVerifyConfirmationCode(verifyConfirmationCodeRepositorySpy)
     const promise = sut.verify(accountId, confirmationCode)
     await expect(promise).rejects.toThrow(new Error())
   })
