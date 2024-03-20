1d4f7b69f470a007f535252b2e63bb6fc0d5a02d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-20 20:55:50 -0300

    refactor: move sut creation to a factory

--- tests/application/usecases/commands/db-change-profile-image.spec.ts
+++ tests/application/usecases/commands/db-change-profile-image.spec.ts
@@ -4,14 +4,27 @@ import { CheckAccountByIdRepositorySpy } from '@/tests/application/mocks/queries
 import { DbChangeProfileImage } from '@/application/usecases/commands'
 import { AccountNotFoundError } from '@/domain/errors'
 
+interface Sut {
+  sut: DbChangeProfileImage
+  checkAccountByIdRepositorySpy: CheckAccountByIdRepositorySpy
+}
+
+const makeSut = (): Sut => {
+  const checkAccountByIdRepositorySpy = new CheckAccountByIdRepositorySpy()
+  const sut = new DbChangeProfileImage(checkAccountByIdRepositorySpy)
+  return {
+    sut,
+    checkAccountByIdRepositorySpy
+  }
+}
+
 const accountId: string = faker.string.uuid()
 const newProfileImage: string = faker.internet.url()
 
 describe('DbChangeProfileImage', () => {
   describe('CheckAccountByIdRepository', () => {
     test('Should call CheckAccountByIdRepository with correct id', async() => {
-      const checkAccountByIdRepositorySpy = new CheckAccountByIdRepositorySpy()
-      const sut = new DbChangeProfileImage(checkAccountByIdRepositorySpy)
+      const { sut, checkAccountByIdRepositorySpy } = makeSut()
 
       await sut.change(accountId, newProfileImage)
 
@@ -19,9 +32,8 @@ describe('DbChangeProfileImage', () => {
     })
 
     test('Should throw AccountNotFoundError if CheckAccountByIdRepository returns false', async() => {
-      const checkAccountByIdRepositorySpy = new CheckAccountByIdRepositorySpy()
+      const { sut, checkAccountByIdRepositorySpy } = makeSut()
       checkAccountByIdRepositorySpy.output = false
-      const sut = new DbChangeProfileImage(checkAccountByIdRepositorySpy)
 
       const promise = sut.change(accountId, newProfileImage)
 
@@ -29,9 +41,8 @@ describe('DbChangeProfileImage', () => {
     })
 
     test('Should throw if CheckAccountByIdRepository throws', async() => {
-      const checkAccountByIdRepositorySpy = new CheckAccountByIdRepositorySpy()
+      const { sut, checkAccountByIdRepositorySpy } = makeSut()
       jest.spyOn(checkAccountByIdRepositorySpy, 'check').mockRejectedValueOnce(new Error())
-      const sut = new DbChangeProfileImage(checkAccountByIdRepositorySpy)
 
       const promise = sut.change(accountId, newProfileImage)
 
