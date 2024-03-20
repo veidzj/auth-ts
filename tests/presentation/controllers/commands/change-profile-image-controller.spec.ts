b60e4dd8f5079b4411554f13d0ee493233717ea0
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-20 20:39:09 -0300

    test: ensure ChangeProfileImageController returns badRequest if Validation throws ValidationError

--- tests/presentation/controllers/commands/change-profile-image-controller.spec.ts
+++ tests/presentation/controllers/commands/change-profile-image-controller.spec.ts
@@ -5,6 +5,7 @@ import { ChangeProfileImageSpy } from '@/tests/domain/mocks/commands'
 import { ChangeProfileImageController } from '@/presentation/controllers/commands'
 import { HttpHelper } from '@/presentation/helpers'
 import { AccountNotFoundError } from '@/domain/errors'
+import { ValidationError } from '@/validation/errors'
 
 interface Sut {
   sut: ChangeProfileImageController
@@ -33,8 +34,20 @@ describe('ChangeProfileImageController', () => {
     test('Should call Validation with correct values', async() => {
       const { sut, validationSpy } = makeSut()
       const request = mockRequest()
+
       await sut.handle(request)
+
       expect(validationSpy.input).toEqual(request)
+    })
+
+    test('Should return badRequest if Validation throws ValidationError', async() => {
+      const { sut, validationSpy } = makeSut()
+      const errorMessage = faker.word.words()
+      jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new ValidationError(errorMessage) })
+
+      const httpResponse = await sut.handle(mockRequest())
+
+      expect(httpResponse).toEqual(HttpHelper.badRequest(new ValidationError(errorMessage)))
     })
   })
 
@@ -42,8 +55,9 @@ describe('ChangeProfileImageController', () => {
     test('Should call ChangeProfileImage with correct values', async() => {
       const { sut, changeProfileImageSpy } = makeSut()
       const request = mockRequest()
-      const httpResponse = await sut.handle(request)
-      expect(httpResponse).toEqual(HttpHelper.noContent())
+
+      await sut.handle(request)
+
       expect(changeProfileImageSpy.accountId).toBe(request.accountId)
       expect(changeProfileImageSpy.newProfileImage).toBe(request.newProfileImage)
     })
@@ -51,20 +65,26 @@ describe('ChangeProfileImageController', () => {
     test('Should return notFound if ChangeProfileImage throws AccountNotFoundError', async() => {
       const { sut, changeProfileImageSpy } = makeSut()
       jest.spyOn(changeProfileImageSpy, 'change').mockRejectedValueOnce(new AccountNotFoundError())
+
       const httpResponse = await sut.handle(mockRequest())
+
       expect(httpResponse).toEqual(HttpHelper.notFound(new AccountNotFoundError()))
     })
 
     test('Should return serverError if ChangeProfileImage throws', async() => {
       const { sut, changeProfileImageSpy } = makeSut()
       jest.spyOn(changeProfileImageSpy, 'change').mockRejectedValueOnce(new Error())
+
       const httpResponse = await sut.handle(mockRequest())
+
       expect(httpResponse).toEqual(HttpHelper.serverError(new Error()))
     })
 
     test('Should return noContent on success', async() => {
       const { sut } = makeSut()
+
       const httpResponse = await sut.handle(mockRequest())
+
       expect(httpResponse).toEqual(HttpHelper.noContent())
     })
   })
