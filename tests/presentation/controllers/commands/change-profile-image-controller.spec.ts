a5ff8a089c4e7853b75db4bc6de4614339cf2c2b
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-20 19:58:00 -0300

    test: ensure ChangeProfileImageController calls ChangeProfileImage with correct values

--- tests/presentation/controllers/commands/change-profile-image-controller.spec.ts
+++ tests/presentation/controllers/commands/change-profile-image-controller.spec.ts
@@ -1,10 +1,47 @@
+import { faker } from '@faker-js/faker'
+
 import { ChangeProfileImageController } from '@/presentation/controllers/commands'
 import { HttpHelper } from '@/presentation/helpers'
+import { type ChangeProfileImage } from '@/domain/usecases/commands'
+
+const mockRequest = (): ChangeProfileImageController.Request => ({
+  accountId: faker.string.uuid(),
+  newProfileImage: faker.internet.url()
+})
 
 describe('ChangeProfileImageController', () => {
+  test('Should call ChangeProfileImage with correct values', async() => {
+    class ChangeProfileImageSpy implements ChangeProfileImage {
+      public accountId: string
+      public newProfileImage: string
+
+      public async change(accountId: string, newProfileImage: string): Promise<void> {
+        this.accountId = accountId
+        this.newProfileImage = newProfileImage
+      }
+    }
+    const changeProfileImageSpy = new ChangeProfileImageSpy()
+    const sut = new ChangeProfileImageController(changeProfileImageSpy)
+    const request = mockRequest()
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(HttpHelper.noContent())
+    expect(changeProfileImageSpy.accountId).toBe(request.accountId)
+    expect(changeProfileImageSpy.newProfileImage).toBe(request.newProfileImage)
+  })
+
   test('Should return noContent on success', async() => {
-    const sut = new ChangeProfileImageController()
-    const httpResponse = await sut.handle({})
+    class ChangeProfileImageSpy implements ChangeProfileImage {
+      public accountId: string
+      public newProfileImage: string
+
+      public async change(accountId: string, newProfileImage: string): Promise<void> {
+        this.accountId = accountId
+        this.newProfileImage = newProfileImage
+      }
+    }
+    const changeProfileImageSpy = new ChangeProfileImageSpy()
+    const sut = new ChangeProfileImageController(changeProfileImageSpy)
+    const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(HttpHelper.noContent())
   })
 })
