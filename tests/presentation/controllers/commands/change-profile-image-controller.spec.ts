0b2beb372fc3263d5180741350132998d0e9fd6e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-20 20:40:46 -0300

    test: ensure ChangeProfileImageController returns serverError if Validation throws

--- tests/presentation/controllers/commands/change-profile-image-controller.spec.ts
+++ tests/presentation/controllers/commands/change-profile-image-controller.spec.ts
@@ -49,6 +49,15 @@ describe('ChangeProfileImageController', () => {
 
       expect(httpResponse).toEqual(HttpHelper.badRequest(new ValidationError(errorMessage)))
     })
+
+    test('Should return serverError if Validation throws', async() => {
+      const { sut, validationSpy } = makeSut()
+      jest.spyOn(validationSpy, 'validate').mockImplementationOnce(() => { throw new Error() })
+
+      const httpResponse = await sut.handle(mockRequest())
+
+      expect(httpResponse).toEqual(HttpHelper.serverError(new Error()))
+    })
   })
 
   describe('ChangeProfileImage', () => {
