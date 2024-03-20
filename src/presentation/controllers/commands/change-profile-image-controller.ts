c0981eda41123533c742b6579f2be2eac9236600
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-20 20:24:23 -0300

    feat: ensure ChangeProfileImageController returns notFound if ChangeProfileImage throws AccountNotFoundError

--- src/presentation/controllers/commands/change-profile-image-controller.ts
+++ src/presentation/controllers/commands/change-profile-image-controller.ts
@@ -1,13 +1,21 @@
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type ChangeProfileImage } from '@/domain/usecases/commands'
+import { AccountNotFoundError } from '@/domain/errors'
 
 export class ChangeProfileImageController implements Controller {
   constructor(private readonly changeProfileImage: ChangeProfileImage) {}
 
   public async handle(request: ChangeProfileImageController.Request): Promise<HttpResponse> {
-    await this.changeProfileImage.change(request.accountId, request.newProfileImage)
-    return HttpHelper.noContent()
+    try {
+      await this.changeProfileImage.change(request.accountId, request.newProfileImage)
+      return HttpHelper.noContent()
+    } catch (error) {
+      if (error instanceof AccountNotFoundError) {
+        return HttpHelper.notFound(error)
+      }
+      return HttpHelper.serverError(error as Error)
+    }
   }
 }
 
