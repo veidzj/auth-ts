83036bcfd16b12d27e2761a2244c15fb0c955f2e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-20 20:51:38 -0300

    feat: ensure DbChangeProfileImage throws AccountNotFoundError if CheckAccountByIdRepository returns false

--- src/application/usecases/commands/db-change-profile-image.ts
+++ src/application/usecases/commands/db-change-profile-image.ts
@@ -1,10 +1,14 @@
 import { type CheckAccountByIdRepository } from '@/application/protocols/queries'
 import { type ChangeProfileImage } from '@/domain/usecases/commands'
+import { AccountNotFoundError } from '@/domain/errors'
 
 export class DbChangeProfileImage implements ChangeProfileImage {
   constructor(private readonly checkAccountByIdRepository: CheckAccountByIdRepository) {}
 
   public async change(accountId: string, newProfileImage: string): Promise<void> {
-    await this.checkAccountByIdRepository.check(accountId)
+    const accountExists = await this.checkAccountByIdRepository.check(accountId)
+    if (!accountExists) {
+      throw new AccountNotFoundError()
+    }
   }
 }
