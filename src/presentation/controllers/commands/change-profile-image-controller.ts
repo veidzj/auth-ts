8ac7abcc52f9d538a91fc2c83a26ea5b4896433a
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-20 20:31:40 -0300

    feat: ensure ChangeProfileImageController calls Validation with correct values

--- src/presentation/controllers/commands/change-profile-image-controller.ts
+++ src/presentation/controllers/commands/change-profile-image-controller.ts
@@ -1,13 +1,17 @@
-import { type Controller, type HttpResponse } from '@/presentation/protocols'
+import { type Controller, type HttpResponse, type Validation } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type ChangeProfileImage } from '@/domain/usecases/commands'
 import { AccountNotFoundError } from '@/domain/errors'
 
 export class ChangeProfileImageController implements Controller {
-  constructor(private readonly changeProfileImage: ChangeProfileImage) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly changeProfileImage: ChangeProfileImage
+  ) {}
 
   public async handle(request: ChangeProfileImageController.Request): Promise<HttpResponse> {
     try {
+      this.validation.validate(request)
       await this.changeProfileImage.change(request.accountId, request.newProfileImage)
       return HttpHelper.noContent()
     } catch (error) {
