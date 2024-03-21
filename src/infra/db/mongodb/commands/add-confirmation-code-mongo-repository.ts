79525c9d91d81b4ff2aea506efb66b1c3ae2580a
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-21 15:52:21 -0300

    fix: mongodb error when get collections in constructor

--- src/infra/db/mongodb/commands/add-confirmation-code-mongo-repository.ts
+++ src/infra/db/mongodb/commands/add-confirmation-code-mongo-repository.ts
@@ -2,20 +2,8 @@ import { MongoRepository } from '@/infra/db/mongodb/common'
 import { type AddConfirmationCodeRepository } from '@/application/protocols/commands'
 
 export class AddConfirmationCodeMongoRepository extends MongoRepository implements AddConfirmationCodeRepository {
-  constructor() {
-    super()
-    void this.createTTLIndex()
-  }
-
-  private async createTTLIndex(): Promise<void> {
-    const codeCollection = this.mongoHelper.getCollection('codes')
-    const indexExists = await codeCollection.indexExists('expirationDate_1')
-    if (!indexExists) {
-      await codeCollection.createIndex({ expirationDate: 1 }, { expireAfterSeconds: 600 })
-    }
-  }
-
   public async add(confirmationCode: string, accountId: string): Promise<string> {
+    await this.createTTLIndex()
     const codeCollection = this.mongoHelper.getCollection('codes')
     const expirationDate = new Date()
     expirationDate.setMinutes(expirationDate.getMinutes() + 10)
@@ -25,5 +13,13 @@ export class AddConfirmationCodeMongoRepository extends MongoRepository implemen
       expirationDate
     })
     return query.insertedId.toString()
+  }
+
+  private async createTTLIndex(): Promise<void> {
+    const codeCollection = this.mongoHelper.getCollection('codes')
+    const indexExists = await codeCollection.indexExists('expirationDate_1')
+    if (!indexExists) {
+      await codeCollection.createIndex({ expirationDate: 1 }, { expireAfterSeconds: 600 })
+    }
   }
 }
