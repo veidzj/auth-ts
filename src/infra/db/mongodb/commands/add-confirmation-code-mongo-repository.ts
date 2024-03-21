6cf0485e2ae359a68aaeef557d9eb830454fc11e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-21 11:25:39 -0300

    feat: ensure confirmation codes are deleted after 600 seconds + expired

--- src/infra/db/mongodb/commands/add-confirmation-code-mongo-repository.ts
+++ src/infra/db/mongodb/commands/add-confirmation-code-mongo-repository.ts
@@ -2,6 +2,16 @@ import { MongoRepository } from '@/infra/db/mongodb/common'
 import { type AddConfirmationCodeRepository } from '@/application/protocols/commands'
 
 export class AddConfirmationCodeMongoRepository extends MongoRepository implements AddConfirmationCodeRepository {
+  constructor() {
+    super()
+    void this.createTTLIndex()
+  }
+
+  private async createTTLIndex(): Promise<void> {
+    const codeCollection = this.mongoHelper.getCollection('codes')
+    await codeCollection.createIndex({ expirationDate: 1 }, { expireAfterSeconds: 600 })
+  }
+
   public async add(confirmationCode: string, accountId: string): Promise<string> {
     const codeCollection = this.mongoHelper.getCollection('codes')
     const expirationDate = new Date()
